2142d55905dc8e051113910cf8b6e4e4c6f051cb
Jon Cram <jon@webignition.net>
2012-08-18 15:09:36 +0100

    Display error if new test form is empty

--- src/SimplyTestable/WebClientBundle/Controller/AppController.php
+++ src/SimplyTestable/WebClientBundle/Controller/AppController.php
@@ -9,7 +9,21 @@ class AppController extends BaseViewController
     public function indexAction()
     {        
         return $this->render('SimplyTestableWebClientBundle:App:index.html.twig', array(
-            'test_start_url' => $this->generateUrl('test_start')
+            'test_start_url' => $this->generateUrl('test_start'),
+            'test_start_error' => $this->hasFlash('test_start_error')
         ));
     }
+    
+    
+    /**
+     *
+     * @param string $flashKey
+     * @return boolean
+     */
+    private function hasFlash($flashKey) {
+        $flashMessages = $this->get('session')->getFlashBag()->get($flashKey);
+        return is_array($flashMessages) && count($flashMessages) > 0;
+    }
+    
+    
 }
--- src/SimplyTestable/WebClientBundle/Controller/TestController.php
+++ src/SimplyTestable/WebClientBundle/Controller/TestController.php
@@ -2,13 +2,47 @@
 
 namespace SimplyTestable\WebClientBundle\Controller;
 
+use webignition\NormalisedUrl\NormalisedUrl;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TestController extends Controller
 {
+    const DEFAULT_WEBSITE_SCHEME = 'http';
+    
     public function startAction()
     {
-        var_dump("cp01");
-        exit();
+        if (!$this->hasWebsite()) {
+            $this->get('session')->setFlash('test_start_error', 'non-blank string');
+            return $this->redirect($this->generateUrl('app', array(), true));
+        }
+    }
+    
+    
+    /**
+     *
+     * @return boolean
+     */
+    private function hasWebsite() {
+        return $this->getWebsite() != '';
+    }
+    
+    
+    /**
+     *
+     * @return string
+     */
+    private function getWebsite() {        
+        $website = trim($this->get('request')->request->get('website'));
+        if ($website == '') {
+            return $website;
+        }
+        
+        $url = new NormalisedUrl($website);
+        if (!$url->hasScheme()) {            
+            $url->setScheme(self::DEFAULT_WEBSITE_SCHEME);
+        }
+        
+        return (string)$url;
     }
 }
