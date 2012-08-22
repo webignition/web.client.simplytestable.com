b81cfe9a7866de57b53ccbee1a53eb72653bde49
Jon Cram <jon@webignition.net>
2012-08-23 00:23:36 +0100

    Pass output=json when redirecting if present

--- src/SimplyTestable/WebClientBundle/Controller/BaseViewController.php
+++ src/SimplyTestable/WebClientBundle/Controller/BaseViewController.php
@@ -5,6 +5,8 @@ namespace SimplyTestable\WebClientBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
+use webignition\NormalisedUrl\NormalisedUrl;
+
 abstract class BaseViewController extends Controller
 {    
     private $template;
@@ -33,6 +35,29 @@ abstract class BaseViewController extends Controller
      */
     private function isJsonResponseRequired() {
         return $this->get('request')->get('output') == 'json';   
+    }
+    
+    
+    /**
+     *
+     * @param string $url
+     * @param int $status
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse 
+     */
+    public function redirect($url, $status = 302) {        
+        if ($this->isJsonResponseRequired()) {
+            $normalisedUrl = new NormalisedUrl($url);
+
+            if ($normalisedUrl->hasQuery()) {                
+                $normalisedUrl->getQuery()->set('output', 'json');
+            } else {
+                $normalisedUrl->setQuery('output=json');
+            }            
+            
+            $url = (string)$normalisedUrl;
+        }
+        
+        return parent::redirect($url, $status);
     }
     
 }
