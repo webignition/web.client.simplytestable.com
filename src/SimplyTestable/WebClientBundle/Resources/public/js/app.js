b49c9b35c3af9345af26a7cf39e4a9ae8f533276
Jon Cram <jon@webignition.net>
2012-08-23 00:31:02 +0100

    Animate progress bar as test progresses

---
+++ src/SimplyTestable/WebClientBundle/Resources/public/js/app.js
@@ -0,0 +1,138 @@
+var application = {};
+
+application.testProgressController = function () {
+    var refresh = function () {
+        jQuery.ajax({
+            complete:function (request, textStatus) {
+                //console.log('complete', request, textStatus);
+            },
+            dataType:'json',
+            error:function (request, textStatus, errorThrown) {
+                console.log('error', request, textStatus, errorThrown);
+            },
+            statusCode: {
+                403: function () {
+                    console.log('403');
+                },
+                500: function () {
+                    console.log('500');
+                }
+            },
+            success: function (data, textStatus, request) {
+                if (data.this_url != window.location.href) {
+                    window.location.href = data.this_url;
+                    return;
+                }
+                
+                var completionPercentValue = $('#completion-percent-value');
+                if (completionPercentValue.text() != data.completion_percent) {
+                    completionPercentValue.text(data.completion_percent);
+                    
+                    $('#completion-percent-bar').css({
+                        'width':data.completion_percent + '%'
+                    });
+                }
+                
+                window.setTimeout(function () {
+                    refresh();
+                }, 1000);
+            },
+            url:window.location.href + '?output=json'
+        });
+    };
+    
+    this.initialise = function () {
+        refresh();
+    };
+};
+
+application.pages = {
+    '/*':{
+        'initialise':function () {
+            if ($('body.app-progress').length > 0) {                
+                testProgressController = new application.testProgressController();
+                testProgressController.initialise();
+            }
+        }         
+    },
+    '/':{
+        'initialise':function () {            
+        }
+    }    
+};
+
+
+
+var applicationController = function () {
+    var getPagePath = function () {
+        var pagePath = window.location.pathname;
+        if (pagePath.substr(pagePath.length - 1, 1)  == '/') {
+            pagePath = pagePath.substr(0, pagePath.length - 1);
+        }        
+        
+        return pagePath;                
+    };
+    
+    var getPagePathParts = function () {
+        return getPagePath().split('/');
+    };
+    
+    var getPageInitialisationPaths = function () {
+        var pageInitialisationPaths = [];
+        var pagePathParts = getPagePathParts();
+        
+        if (pagePathParts.length === 1) {
+            return [
+                '/',
+                '/*'
+            ];
+        }
+        
+        var currentPath = '';
+        var isFirstPath = true;
+        
+        while (pagePathParts.length > 0) {
+            currentPath = pagePathParts.join('/');            
+            if (!isFirstPath) {
+                currentPath += '/*';
+            }
+            
+            pageInitialisationPaths.push(currentPath);            
+
+            pagePathParts = pagePathParts.slice(0, pagePathParts.length - 1);
+            isFirstPath = false;
+        }
+        
+        return pageInitialisationPaths;
+    };    
+    
+    var getInitialisationMethods = function () {
+        var pageInitialisationPaths = getPageInitialisationPaths();
+        var initialisationMethods = [];
+        
+        for (var initialisationPathIndex = 0; initialisationPathIndex < pageInitialisationPaths.length; initialisationPathIndex++) {            
+            if (typeof application.pages[pageInitialisationPaths[initialisationPathIndex]] == 'object') {
+                if (typeof application.pages[pageInitialisationPaths[initialisationPathIndex]]['initialise'] == 'function') {
+                    initialisationMethods.push(application.pages[pageInitialisationPaths[initialisationPathIndex]]['initialise']);
+                }
+            }
+        }
+        
+        return initialisationMethods;        
+    };    
+    
+    var initialise = function () {
+        var initialisationMethods = getInitialisationMethods();        
+        for (var initialisationMethodIndex = 0; initialisationMethodIndex < initialisationMethods.length; initialisationMethodIndex++) {
+            initialisationMethods[initialisationMethodIndex]();
+        }
+    };    
+    
+    this.initialise = initialise;
+};
+
+
+$(document).ready(function() {
+    var app = new applicationController();
+    app.initialise();
+});
