e4829d2856b887748534716a501f89eb4d0eeb2a
Jon Cram <jon@webignition.net>
2013-03-13 11:35:07 +0000

    Added /{website}/queued/status/

--- src/SimplyTestable/WebClientBundle/Controller/TestController.php
+++ src/SimplyTestable/WebClientBundle/Controller/TestController.php
@@ -4,6 +4,21 @@ namespace SimplyTestable\WebClientBundle\Controller;
 
 class TestController extends BaseController
 {    
+    /**
+     *
+     * @var \SimplyTestable\WebClientBundle\Services\TestQueueService
+     */
+    private $testQueueService;    
+    
+    
+    public function queuedStatusAction($website) {        
+        $normalisedWebsite = new \webignition\NormalisedUrl\NormalisedUrl($website);        
+        $result = ($this->getTestQueueService()->contains($this->getUser(), (string)$normalisedWebsite)) ? 'queued' : 'not queued';
+        
+        return new \Symfony\Component\HttpFoundation\Response($result);       
+    }
+    
+    
     public function cancelAction()
     {
         $this->getTestService()->setUser($this->getUser());
@@ -64,5 +79,21 @@ class TestController extends BaseController
      */
     private function getTestService() {
         return $this->container->get('simplytestable.services.testservice');
-    }  
+    } 
+    
+    
+    /**
+     *
+     * @return \SimplyTestable\WebClientBundle\Services\TestQueueService
+     */
+    private function getTestQueueService() {
+        if (is_null($this->testQueueService)) {
+            $this->testQueueService = $this->container->get('simplytestable.services.testqueueservice');
+            $this->testQueueService->setApplicationRootDirectory($this->container->get('kernel')->getRootDir());
+                    
+        }
+        
+        return $this->testQueueService;
+
+    }     
 }
