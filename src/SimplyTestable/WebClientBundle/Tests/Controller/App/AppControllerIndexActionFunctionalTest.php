12a0d09722c3f8962c5b98208401084dca0130db
Jon Cram <jon@webignition.net>
2013-04-16 16:31:24 +0100

    Removed test cruft

--- src/SimplyTestable/WebClientBundle/Tests/Controller/App/AppControllerIndexActionFunctionalTest.php
+++ src/SimplyTestable/WebClientBundle/Tests/Controller/App/AppControllerIndexActionFunctionalTest.php
@@ -16,29 +16,7 @@ class AppControllerIndexActionFunctionalTest extends ControllerFunctionalTest {
         /* @var $crawler \Symfony\Component\DomCrawler\Crawler */
         $crawler = $this->client->request('GET', self::INDEX_ACTION_ROUTE);
         
-        $this->publicUserNavbarContentTest($crawler);
-        
-        //var_dump($crawler->filter('.navbar')->count());
-        
-        /* @var $navbar \Symfony\Component\DomCrawler\Crawler */
-        //$navbar = $crawler->filter('.navbar');
-        
-        //var_dump($navbar->filter('a:contains("Sign in")')->text());
-        
-        //$navbar->filter('a:contains("Sign in")');
-        
-        
-//        
-//        var_dump($navbar);
-//        
-//        $link = $navbar->filter('a:contains("Sign in")')->eq(1)->link();
-//        
-//        var_dump($link);
-        
-//            $this->assertGreaterThan(
-//                0,
-//                $crawler->filter('html:contains("Hello Fabien")')->count()
-//            );        
+        $this->publicUserNavbarContentTest($crawler);     
     }
 }
 
