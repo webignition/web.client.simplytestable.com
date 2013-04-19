f51a68518b76b84653a946346a9cbf69c44ec869
Jon Cram <jon@webignition.net>
2013-04-19 09:17:02 +0100

    Add ControllerTestRetrievalOutcomeTest model

---
+++ src/SimplyTestable/WebClientBundle/Model/ControllerTestRetrievalOutcome.php
@@ -0,0 +1,58 @@
+<?php
+namespace SimplyTestable\WebClientBundle\Model;
+
+class ControllerTestRetrievalOutcome { 
+
+    /**
+     *
+     * @var \Symfony\Component\HttpFoundation\Response
+     */
+    private $response;
+    
+    
+    /**
+     *
+     * @var \SimplyTestable\WebClientBundle\Entity\Test\Test
+     */
+    private $test;
+    
+    
+    /**
+     * 
+     * @param \Symfony\Component\HttpFoundation\Response $response
+     * @return \SimplyTestable\WebClientBundle\Model\ControllerTestRetrievalOutcome
+     */
+    public function setResponse(\Symfony\Component\HttpFoundation\Response $response) {
+        $this->response = $response;
+        return $this;
+    }
+    
+    /**
+     * 
+     * @param \SimplyTestable\WebClientBundle\Entity\Test\Test $test
+     * @return \SimplyTestable\WebClientBundle\Model\ControllerTestRetrievalOutcome
+     */
+    public function setTest(\SimplyTestable\WebClientBundle\Entity\Test\Test $test) {
+        $this->test = $test;
+        return $this;
+    }
+    
+
+    /**
+     * 
+     * @return boolean
+     */    
+    public function hasTest() {
+        return !is_null($this->test);
+    }
+    
+    
+    /**
+     * 
+     * @return boolean
+     */
+    public function hasResponse() {
+        return !is_null($this->response);
+    }
+    
+}
