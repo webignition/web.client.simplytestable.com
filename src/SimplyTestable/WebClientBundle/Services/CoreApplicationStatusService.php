0922d3c9c0dd4e217d3afb1372bbf5a2fc5d9259
Jon Cram <jon@webignition.net>
2013-04-23 16:04:55 +0100

    Remove cruft

--- src/SimplyTestable/WebClientBundle/Services/CoreApplicationStatusService.php
+++ src/SimplyTestable/WebClientBundle/Services/CoreApplicationStatusService.php
@@ -69,39 +69,4 @@ class CoreApplicationStatusService extends CoreApplicationService {
     }
     
     
-    
-    
-    //public function 
-    
-    
-//    /**
-//     *
-//     * @param string $canonicalUrl
-//     * @param int $testId
-//     * @return boolean 
-//     */
-//    public function cancel($canonicalUrl, $testId) {
-//        $httpRequest = $this->getAuthorisedHttpRequest(
-//            $this->getUrl('test_cancel', array(
-//            'canonical-url' => $canonicalUrl,
-//            'test_id' => $testId
-//        )));
-//        
-//        /* @var $response \webignition\WebResource\JsonDocument\JsonDocument */
-//        try {
-//            $this->webResourceService->get($httpRequest);
-//            return true;
-//        } catch (\webignition\Http\Client\CurlException $curlException) {
-//            $this->logger->warn('TestService::cancel:curlException: ['.$curlException->getCode().'] ['.$curlException->getMessage().']');
-//            return $curlException;
-//        } catch (\SimplyTestable\WebClientBundle\Exception\WebResourceServiceException $webResourceServiceException) {                      
-//            $this->logger->warn('TestService::cancel:WebResourceServiceException: ['.$webResourceServiceException->getCode().'] ['.$webResourceServiceException->getMessage().']');
-////            if ($webResourceServiceException->getCode() == 403) {
-////                return false;
-////            }            
-//            return $webResourceServiceException->getCode();
-//        }        
-//    }
-    
-    
 }
