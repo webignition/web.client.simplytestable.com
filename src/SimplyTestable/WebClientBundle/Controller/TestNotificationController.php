a9806976a80b9e395822d2acc822714f6d866e66
Jon Cram <jon@webignition.net>
2013-07-12 23:15:53 +0100

    Present url-limited notification as standalone content

---
+++ src/SimplyTestable/WebClientBundle/Controller/TestNotificationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SimplyTestable\WebClientBundle\Controller;
+
+class TestNotificationController extends TestViewController
+{
+    public function urlLimitAction($test_id, $website) {
+        $testRetrievalOutcome = $this->getTestRetrievalOutcome($website, $test_id);
+        if ($testRetrievalOutcome->hasResponse()) {
+            return $this->sendNotFoundResponse();
+        }
+        
+        $this->setTemplate('SimplyTestableWebClientBundle:Partials:test-url-limit-notification.html.twig');
+        return $this->sendResponse(array(
+            'is_logged_in' => !$this->getUserService()->isPublicUser($this->getUser()),
+            'remote_test_summary' => $this->getTestService()->getRemoteTestSummary()
+        ));        
+    }
+}
