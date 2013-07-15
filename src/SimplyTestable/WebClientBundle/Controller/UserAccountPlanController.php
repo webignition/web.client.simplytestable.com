f65f541e624c9efd705e7952e7230ec0f4010956
Jon Cram <jon@webignition.net>
2013-07-15 11:26:58 +0100

    Live debug logging

--- src/SimplyTestable/WebClientBundle/Controller/UserAccountPlanController.php
+++ src/SimplyTestable/WebClientBundle/Controller/UserAccountPlanController.php
@@ -15,18 +15,21 @@ class UserAccountPlanController extends AbstractUserAccountController
         
         $userSummary = $this->getUserService()->getSummary($this->getUser())->getContentObject();
         if (isset($userSummary->user_plan) && $userSummary->user_plan->plan->name == $this->get('request')->request->get('plan')) {
+            $this->get('logger')->err('UserAccountPlanController::subscribeAction::already on selected plan');
             $this->get('session')->setFlash('plan_subscribe_success', 'already-on-plan');
             return $redirectResponse;
         }        
 
         try {
             $response = $this->getUserPlanSubscriptionService()->subscribe($this->getUser(), $this->get('request')->request->get('plan'));
+            $this->get('logger')->err('UserAccountPlanController::subscribeAction::subscribe method done');
             if ($response === true) {
                 $this->get('session')->setFlash('plan_subscribe_success', 'ok');
             } else {
                 $this->get('session')->setFlash('plan_subscribe_error', $response);            
             } 
         } catch (\SimplyTestable\WebClientBundle\Exception\UserAccountCardException $userAccountCardException) {
+            $this->get('logger')->err('UserAccountPlanController::subscribeAction::stripe card error');
             $this->get('session')->setFlash('user_account_card_exception_message', $userAccountCardException->getMessage());
             $this->get('session')->setFlash('user_account_card_exception_param', $userAccountCardException->getParam());
             $this->get('session')->setFlash('user_account_card_exception_code', $userAccountCardException->getStripeCode());
