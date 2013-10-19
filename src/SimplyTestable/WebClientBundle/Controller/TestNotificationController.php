6f93ce22ffc829a28959883d90a7b40cd5d310ad
Jon Cram <jon@webignition.net>
2013-10-19 10:11:13 +0100

    Set user for core app service correctly

--- src/SimplyTestable/WebClientBundle/Controller/TestNotificationController.php
+++ src/SimplyTestable/WebClientBundle/Controller/TestNotificationController.php
@@ -5,12 +5,12 @@ namespace SimplyTestable\WebClientBundle\Controller;
 class TestNotificationController extends TestViewController
 {
     public function urlLimitAction($test_id, $website) {
+        $this->getTestService()->setUser($this->getUser());
+        
         $testRetrievalOutcome = $this->getTestRetrievalOutcome($website, $test_id);
         if ($testRetrievalOutcome->hasResponse()) {
             return $this->sendNotFoundResponse();
-        }
-        
-        $this->getTestService()->setUser($this->getUser());
+        }        
         
         $this->setTemplate('SimplyTestableWebClientBundle:Partials:test-url-limit-notification.html.twig');
         return $this->sendResponse(array(
