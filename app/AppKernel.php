cc36f8592e8b373730d6ef0a019e6b9d334bfa87
Jon Cram <jon@webignition.net>
2014-02-20 10:25:14 +0000

    Use just   = ->get('postmark.message');

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle($this),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Glit\ResqueBundle\GlitResqueBundle(),
+            new MZ\PostmarkBundle\MZPostmarkBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
