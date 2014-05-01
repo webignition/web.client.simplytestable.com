3d32f2975680664d7aa6da42622bada08f35ff3e
Jon Cram <jon@webignition.net>
2014-05-01 14:24:40 +0100

    Handle mailchimp list recipients service exceptions in mailchimp webhook receiver

--- src/SimplyTestable/WebClientBundle/Controller/MailChimp/EventController.php
+++ src/SimplyTestable/WebClientBundle/Controller/MailChimp/EventController.php
@@ -25,10 +25,45 @@ class EventController extends BaseController {
             return new Response('', 400);
         }
         
-        $this->getDispatcher()->dispatch(
-                $this->getListenerEventName(),
-                $this->getEvent()
-        );   
+        try {
+            $this->getDispatcher()->dispatch(
+                    $this->getListenerEventName(),
+                    $this->getEvent()
+            );            
+        } catch (\DomainException $domainException) {
+            // Invalid event data list_id
+            if ($domainException->getCode() === 2) {
+                $eventData = $this->get('request')->request->get('data');
+                
+                $logMessage = 'Controller\MailChimp\EventController ';
+                
+                $logMessage .= (array_key_exists('list_id', $eventData))
+                    ? 'invalid list_id "' . $eventData['list_id'] . '" in event data'
+                    : 'no list_id in event data';
+                
+                $this->getLogger()->error('Controller\MailChimp\EventController \DomainException START');
+                $this->getLogger()->error($logMessage);
+                $this->getLogger()->error($domainException);
+                $this->getLogger()->error($domainException);                
+                $this->getLogger()->error('Controller\MailChimp\EventController \DomainException END');
+            } else {
+                throw $domainException;
+            }
+
+        } catch (\UnexpectedValueException $unexpectedValueException) {
+            // Event raw data is missing "data"
+            if ($unexpectedValueException->getCode() === 2) {
+                $this->getLogger()->error('Controller\MailChimp\EventController \UnexpectedValueException START');                
+                $this->getLogger()->error('Controller\MailChimp\EventController event raw data is missing "data"');
+                $this->getLogger()->error($this->get('request'));
+                $this->getLogger()->error($unexpectedValueException);                
+                $this->getLogger()->error('Controller\MailChimp\EventController \UnexpectedValueException END');                
+                
+
+            } else {
+                throw $unexpectedValueException;
+            }
+        }
         
         return new Response('', 200);
     }
