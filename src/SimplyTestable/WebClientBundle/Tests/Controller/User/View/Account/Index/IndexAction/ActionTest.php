5e9a342067b4a3340252dc61892da51ea2800c2e
Jon Cram <jon@webignition.net>
2014-05-05 08:13:39 +0100

    Remove old UserAccountController

---
+++ src/SimplyTestable/WebClientBundle/Tests/Controller/User/View/Account/Index/IndexAction/ActionTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SimplyTestable\WebClientBundle\Tests\Controller\User\View\ResetPassword\Index\IndexAction;
+
+use SimplyTestable\WebClientBundle\Tests\Controller\Base\ActionTest as BaseActionTest;
+
+class ActionTest extends BaseActionTest {        
+  
+    protected function getExpectedResponseStatusCode() {
+        return 200;
+    }
+
+}
+
+
+ 
