7828621db695a1f9bc8fbbafb928f0e6e81087e9
Jon Cram <jon@webignition.net>
2014-05-19 17:23:24 +0100

    Add task results controller interface tests

---
+++ src/SimplyTestable/WebClientBundle/Tests/Controller/View/Test/Results/Preparing/Index/IndexAction/RequiresValidOwnerTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace SimplyTestable\WebClientBundle\Tests\Controller\View\Test\Results\Preparing\Index\IndexAction;
+
+use SimplyTestable\WebClientBundle\Tests\Controller\Base\RequiresValidOwnerTest as BaseRequiresValidOwnerTest;
+
+class RequiresValidOwnerTest extends BaseRequiresValidOwnerTest {
+}
