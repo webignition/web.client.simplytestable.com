a8a7aa7db0a4b323a8a8bcace55779c2a278773c
Jon Cram <jon@webignition.net>
2014-05-21 08:41:33 +0100

    Add new bare test results controller and basic routing and interface tests

---
+++ src/SimplyTestable/WebClientBundle/Controller/View/Test/Results/IndexController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SimplyTestable\WebClientBundle\Controller\View\Test\Results;
+
+use SimplyTestable\WebClientBundle\Controller\View\Test\CacheableViewController;
+use SimplyTestable\WebClientBundle\Interfaces\Controller\IEFiltered;
+use SimplyTestable\WebClientBundle\Interfaces\Controller\RequiresValidUser;
+use SimplyTestable\WebClientBundle\Interfaces\Controller\Test\RequiresValidOwner;
+
+class IndexController extends CacheableViewController implements IEFiltered, RequiresValidUser, RequiresValidOwner {
+
+
+    protected function modifyViewName($viewName) {
+        return str_replace(array(
+            ':Test',
+        ), array(
+            ':bs3/Test',
+        ), $viewName);
+    }
+
+
+    public function indexAction($website, $test_id) {
+        $viewData = array();
+
+        return $this->renderCacheableResponse($viewData);
+    }
+
+    public function getCacheValidatorParameters() {
+        return array(
+            'rand' => rand()
+        );
+    }
+
+}
---
+++ src/SimplyTestable/WebClientBundle/Tests/Controller/View/Test/Results/Index/IndexAction/RequiresValidOwnerTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace SimplyTestable\WebClientBundle\Tests\Controller\View\Test\Results\Index\IndexAction;
+
+use SimplyTestable\WebClientBundle\Tests\Controller\Base\RequiresValidOwnerTest as BaseRequiresValidOwnerTest;
+
+class RequiresValidOwnerTest extends BaseRequiresValidOwnerTest {
+}
