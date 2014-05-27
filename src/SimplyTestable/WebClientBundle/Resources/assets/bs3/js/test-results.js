d9d86a15d6c4b68b641f64f0408bbef0cf087bfd
Jon Cram <jon@webignition.net>
2014-05-27 15:28:41 +0100

    Correctly place test results side action buttons for sm

--- src/SimplyTestable/WebClientBundle/Resources/assets/bs3/js/test-results.js
+++ src/SimplyTestable/WebClientBundle/Resources/assets/bs3/js/test-results.js
@@ -1,4 +1,99 @@
 $(document).ready(function() {
+    var getDeviceSize = function () {
+        if ($('#device-size-lg').css('visibility') === 'visible') {
+            return 'lg';
+        }
+
+        if ($('#device-size-md').css('visibility') === 'visible') {
+            return 'md';
+        }
+
+        if ($('#device-size-sm').css('visibility') === 'visible') {
+            return 'sm';
+        }
+
+        if ($('#device-size-xs').css('visibility') === 'visible') {
+            return 'xs';
+        }
+    }
+
+    var sideActionTopDefaults = {};
+
+    var storeSideActionTopDefaults = function () {
+        $('.side-action').each(function () {
+            var item = $(this);
+
+            sideActionTopDefaults[item.attr('id')] = item.css('top');
+        });
+    };
+
+    var setSideActionElementsAtSameOffsetAsTestSummary = function () {
+        var summaryStatsOffset = $('.summary-stats').offset().top;
+
+        $('.side-action').each(function () {
+            var item = $(this);
+            item.show().css({
+                'visibility':'hidden'
+            });
+
+            var offset = item.offset();
+            var diff = summaryStatsOffset + parseInt(item.attr('data-offset'), 10) - 147;
+
+            item.css({
+                'top':diff + 'px',
+                'visibility':'visible'
+            });
+        });
+    };
+
+    storeSideActionTopDefaults();
+
+    $( window ).resize(function() {
+
+        console.log(getDeviceSize());
+
+        if (getDeviceSize() === 'lg' || getDeviceSize() === 'md') {
+            $($('.side-action').get(0)).css({
+                'top':'0px'
+            });
+
+            $($('.side-action').get(1)).css({
+                'top':'70px'
+            });
+
+            if ($('.side-action').length === 3) {
+                $($('.side-action').get(2)).css({
+                    'top':'140px'
+                });
+            }
+        }
+
+        if (getDeviceSize() === 'sm') {
+            $($('.side-action').get(0)).css({
+                'top':'145px'
+            });
+
+            $($('.side-action').get(1)).css({
+                'top':'215px'
+            });
+
+            if ($('.side-action').length === 3) {
+                $($('.side-action').get(2)).css({
+                    'top':'285px'
+                });
+            }
+        }
+
+        storeSideActionTopDefaults();
+
+        if (getDeviceSize() === 'sm') {
+            if ($('#test-options.collapse.in').length) {
+                setSideActionElementsAtSameOffsetAsTestSummary();
+            }
+        }
+    });
+
+
     $('.collapse-control').each(function () {
         var control = $(this);
         var detail = $(control.attr('data-target'));
@@ -11,6 +106,7 @@ $(document).ready(function() {
             control.append(' <i class="fa fa-caret-up"></i>');
 
             event.preventDefault();
+            event.stopPropagation();
         });
 
         detail.on('hidden.bs.collapse', function (event) {
@@ -21,6 +117,7 @@ $(document).ready(function() {
             control.append(' <i class="fa fa-caret-down"></i>');
 
             event.preventDefault();
+            event.stopPropagation();
         });
     });
 
@@ -58,5 +155,64 @@ $(document).ready(function() {
             'offset':-100
         });
     }
+
+    $('#test-options').on('show.bs.collapse', function (event) {
+        if ($(event.target).attr('id') !== 'test-options') {
+            return;
+        }
+
+        if (getDeviceSize() !== 'sm') {
+            return;
+        }
+
+        $('.side-action').hide();
+    });
+
+    $('#test-options').on('shown.bs.collapse', function (event) {
+        if ($(event.target).attr('id') !== 'test-options') {
+            return;
+        }
+
+        if (getDeviceSize() !== 'sm') {
+            return;
+        }
+
+        setSideActionElementsAtSameOffsetAsTestSummary();
+    });
+
+    $('#test-options').on('hide.bs.collapse', function (event) {
+        if ($(event.target).attr('id') !== 'test-options') {
+            return;
+        }
+
+        if (getDeviceSize() !== 'sm') {
+            return;
+        }
+
+        $('.side-action').hide();
+    });
+
+    $('#test-options').on('hidden.bs.collapse', function (event) {
+        if ($(event.target).attr('id') !== 'test-options') {
+            return;
+        }
+
+        if (getDeviceSize() !== 'sm') {
+            return;
+        }
+
+        $('.side-action').each(function () {
+            var item = $(this);
+
+            item.show().css({
+                'visibility':'hidden'
+            })
+
+            item.css({
+                'top': sideActionTopDefaults[item.attr('id')],
+                'visibility':'visible'
+            });
+        });
+    });
 
 });
