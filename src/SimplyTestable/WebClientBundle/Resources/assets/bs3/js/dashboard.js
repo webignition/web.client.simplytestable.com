d4efcfe8ec843d672945c60dd0b15c4f2cb060f6
Jon Cram <jon@webignition.net>
2014-06-12 11:40:19 +0100

    Add js static analysis advanced opts

--- src/SimplyTestable/WebClientBundle/Resources/assets/bs3/js/dashboard.js
+++ src/SimplyTestable/WebClientBundle/Resources/assets/bs3/js/dashboard.js
@@ -1,22 +1,40 @@
 $(document).ready(function() {
-    $('.collapse-control').each(function () {
+    $('.collapse-control-group .collapse-control').each(function () {
         var control = $(this);
         var detail = $(control.attr('data-target'));
+
+        detail.on('show.bs.collapse', function (event) {
+            var target = $(event.target);
+            var control = $('[data-target=#' + target.attr('id') + ']');
+
+            $('.collapse-control-group .collapse-control').each(function () {
+                var currentControl = $(this);
+
+                if (currentControl.attr('data-target') !== control.attr('data-target')) {
+                    var currentDetail = $(currentControl.attr('data-target'));
+                    if (currentDetail.is('.in')) {
+                        currentDetail.collapse('hide');
+                    }
+                }
+            });
+        });
 
         detail.on('shown.bs.collapse', function (event) {
             var target = $(event.target);
             var control = $('[data-target=#' + target.attr('id') + ']');
 
-            $('.fa', control).remove();
-            control.append(' <i class="fa fa-caret-up"></i>');
+            $('.fa', control).removeClass('fa-caret-up fa-caret-down').addClass('fa-caret-up');
         });
 
         detail.on('hidden.bs.collapse', function (event) {
             var target = $(event.target);
             var control = $('[data-target=#' + target.attr('id') + ']');
 
-            $('.fa', control).remove();
-            control.append(' <i class="fa fa-caret-down"></i>');
+            $('.fa', control).removeClass('fa-caret-up fa-caret-down').addClass('fa-caret-down');
+        });
+
+        detail.on('hide.bs.collapse', function (event) {
+
         });
     });
 
