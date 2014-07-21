0d8a086a8ea4cc2c2938d06ef69e1cbd433e0cd4
Jon Cram <jon@webignition.net>
2014-07-21 17:28:22 +0100

    Make team info detail expandable

--- src/SimplyTestable/WebClientBundle/Resources/assets/bs3/js/user-account-team.js
+++ src/SimplyTestable/WebClientBundle/Resources/assets/bs3/js/user-account-team.js
@@ -15,4 +15,21 @@ $(document).ready(function() {
         }
     }
 
+    $('.collapse-control').each(function () {
+        var control = $(this);
+        var detail = $(control.attr('data-target'));
+
+        control.append('<i class="fa fa-caret-down"></i>');
+
+        detail.on('shown.bs.collapse', function () {
+            $('.fa', control).remove();
+            control.append('<i class="fa fa-caret-up"></i>');
+        });
+
+        detail.on('hidden.bs.collapse', function () {
+            $('.fa', control).remove();
+            control.append('<i class="fa fa-caret-down"></i>');
+        });
+    });
+
 });
