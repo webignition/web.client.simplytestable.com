189cbb268e5a226a2054a11a4500d5f607695f1c
Jon Cram <jon@webignition.net>
2014-07-25 09:26:40 +0100

    Focus invite field only when relevant

--- src/SimplyTestable/WebClientBundle/Resources/assets/bs3/js/user-account-team.js
+++ src/SimplyTestable/WebClientBundle/Resources/assets/bs3/js/user-account-team.js
@@ -8,11 +8,27 @@ $(document).ready(function() {
     }
 
     if ($('body.in-team.team-leader').length === 1) {
-
         var input = $('#email');
-        if (input.stFormHelper().isEmpty() || input.stFormErrorHelper().hasError()) {
-            input.stFormHelper().select();
+
+        if (input.is('.focus')) {
+            if (input.stFormHelper().isEmpty() || input.stFormErrorHelper().hasError()) {
+                input.stFormHelper().select();
+            }
         }
+
+        if ($('.invite').length === 0) {
+            if (input.stFormHelper().isEmpty() || input.stFormErrorHelper().hasError()) {
+                input.stFormHelper().select();
+            }
+        }
+
+        $('#sidenav').on('activate.bs.scrollspy', function (event) {
+            if ($('a', event.target).attr('href') == '#invites') {
+                if (input.stFormHelper().isEmpty() || input.stFormErrorHelper().hasError()) {
+                    input.stFormHelper().select();
+                }
+            }
+        })
     }
 
     $('.collapse-control').each(function () {
