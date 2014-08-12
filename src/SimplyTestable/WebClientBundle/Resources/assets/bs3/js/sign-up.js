a18b581e3207c09a86719e8146494d5be6a69694
Jon Cram <jon@webignition.net>
2014-08-12 16:45:54 +0100

    Disable signup button and add feedback on click

--- src/SimplyTestable/WebClientBundle/Resources/assets/bs3/js/sign-up.js
+++ src/SimplyTestable/WebClientBundle/Resources/assets/bs3/js/sign-up.js
@@ -18,5 +18,14 @@ $(document).ready(function() {
         $('.plan.checked').removeClass('checked');
 
         $(this).closest('.plan').addClass('checked');
-    })
+    });
+
+    $('button[type=submit]').click(function () {
+        var button = $(this);
+
+        $('.fa', button).removeClass('fa-caret-right').addClass('fa-spinner fa-spin');
+        button.animate({
+            'opacity':0.6
+        });
+    });
 });
