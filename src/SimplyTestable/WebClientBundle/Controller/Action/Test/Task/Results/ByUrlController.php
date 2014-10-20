107f4e9cf744a16eb30c821877b1128195edfa57
Jon Cram <jon@webignition.net>
2014-10-20 21:46:57 +0100

    Add redirect to access task results by task url

---
+++ src/SimplyTestable/WebClientBundle/Controller/Action/Test/Task/Results/ByUrlController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace SimplyTestable\WebClientBundle\Controller\Action\Test\Task\Results;
+
+use SimplyTestable\WebClientBundle\Controller\BaseController;
+
+class ByUrlController extends BaseController {
+
+    public function indexAction($website, $test_id, $task_url) {
+        $test = $this->getTestService()->getEntityRepository()->findOneBy([
+            'testId' => $test_id
+        ]);
+
+        $task = $this->getTaskService()->getEntityRepository()->findOneBy([
+            'test' => $test,
+            'url' => $task_url
+        ]);
+
+        if (is_null($task)) {
+            return $this->redirect($this->generateUrl('app_test_redirector', array(
+                'website' => $website,
+                'test_id' => $test_id
+            )));
+        }
+
+        return $this->redirect($this->generateUrl('view_test_task_results_index_index', [
+            'website' => $website,
+            'test_id' => $test_id,
+            'task_id' => $task->getTaskId()
+        ]));
+    }
+
+
+
+    /**
+     *
+     * @return \SimplyTestable\WebClientBundle\Services\TestService
+     */
+    private function getTestService() {
+        return $this->container->get('simplytestable.services.testservice');
+    }
+
+
+    /**
+     *
+     * @return \SimplyTestable\WebClientBundle\Services\TaskService
+     */
+    protected function getTaskService() {
+        return $this->container->get('simplytestable.services.taskservice');
+    }
+}
