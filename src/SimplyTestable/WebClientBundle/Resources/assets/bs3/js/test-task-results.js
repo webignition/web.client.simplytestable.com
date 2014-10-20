800672ec2d90c50393110d00da2699780fcdc430
Jon Cram <jon@webignition.net>
2014-10-20 21:46:27 +0100

    Allow single error to be highlighted on task results

--- src/SimplyTestable/WebClientBundle/Resources/assets/bs3/js/test-task-results.js
+++ src/SimplyTestable/WebClientBundle/Resources/assets/bs3/js/test-task-results.js
@@ -1,4 +1,73 @@
 $(document).ready(function() {
+    var generateUniqueErrorHash = function (error) {
+        var sourceHash = SparkMD5.hash(error);
+
+        if ($('#' + sourceHash).length === 0) {
+            return sourceHash;
+        }
+
+        var suffixDigit = 1;
+        var modifiedHash = sourceHash + '-' + suffixDigit;
+
+        while ($('#' + modifiedHash).length) {
+            suffixDigit++;
+            modifiedHash = sourceHash + '-' + suffixDigit;
+        }
+
+        return modifiedHash;
+    };
+
+    var issuesList = $('.error-list .issues li, .warning-list .issues li');
+
+    issuesList.each(function () {
+        var issue = $(this);
+        var content = jQuery.trim($('p', issue).text());
+
+        var hash = generateUniqueErrorHash(content);
+        issue.attr('id', hash);
+
+        if (window.location.hash) {
+            var comparator = window.location.hash.replace('#', '');
+
+            if (hash.indexOf(comparator) !== -1) {
+                $('body').addClass('filtered');
+                issue.addClass('highlight');
+            }
+        }
+    });
+
+    if ($('body').is('.filtered') && window.location.hash) {
+        issuesList.not('.highlight').addClass('hidden');
+
+        var fixesList = $('.fixes-list li');
+
+        fixesList.each(function () {
+            var fix = $(this);
+            var content = jQuery.trim($('a', fix).text());
+
+            var hash = generateUniqueErrorHash(content);
+            var comparator = window.location.hash.replace('#', '');
+
+            if (hash.indexOf(comparator) === -1) {
+                fix.addClass('hidden');
+            }
+        });
+
+        var fixCount = $('.fixes-list').not('.hidden').length + '';
+
+        $('.alert-fixes').text(fixCount);
+        $('.label-fixes .count').text(fixCount);
+
+        $('.label-fixes .name').text((fixCount == 1) ? 'fix' : 'fixes');
+
+        var filteredError = $('p', $(issuesList).filter('.highlight').first()).text();
+        console.log(filteredError);
+
+        $('.issue-content').prepend(
+            $('<p class="filter-notice lead">Showing only <span class="message">"' + jQuery.trim(filteredError) + '"</span> errors. <a href="">Show all.</span></p>')
+        );
+    }
+
     $('.summary-stats a').click(function () {
         var target = $($(this).attr('href'));
 
