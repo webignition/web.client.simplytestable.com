5a82af287ca3d47dee071342a887219ae6c22186
Jon Cram <jon@webignition.net>
2017-12-20 13:21:55 +0000

    Test mail service

---
+++ src/SimplyTestable/WebClientBundle/Tests/Functional/Services/Mail/ServiceTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace SimplyTestable\WebClientBundle\Tests\Functional\Services\Mail;
+
+use SimplyTestable\WebClientBundle\Services\Mail\Service as MailService;
+use SimplyTestable\WebClientBundle\Tests\Functional\BaseSimplyTestableTestCase;
+use MZ\PostmarkBundle\Postmark\Message as PostmarkMessage;
+
+class ServiceTest extends BaseSimplyTestableTestCase
+{
+    /**
+     * @var MailService
+     */
+    private $mailService;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->mailService = $this->container->get('simplytestable.services.mail.service');
+    }
+
+    public function testGetConfiguration()
+    {
+        $this->assertEquals(
+            $this->container->get('simplytestable.services.mail.configuration'),
+            $this->mailService->getConfiguration()
+        );
+    }
+
+    public function testGetNewMessage()
+    {
+        $message = $this->mailService->getNewMessage();
+
+        $this->assertInstanceOf(PostmarkMessage::class, $message);
+    }
+
+    public function testGetSender()
+    {
+        $this->assertEquals(
+            $this->container->get('simplytestable.services.postmark.sender'),
+            $this->mailService->getSender()
+        );
+    }
+}
