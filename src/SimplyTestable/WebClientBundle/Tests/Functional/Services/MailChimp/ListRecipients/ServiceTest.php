7327d25dd9e3a577653f7cfefa10828a35ed2810
Jon Cram <webignition@gmail.com>
2017-12-20 16:03:55 +0000

    Remove MailChimp/ListRecipientsService::removeList() (#9)

--- src/SimplyTestable/WebClientBundle/Tests/Functional/Services/MailChimp/ListRecipients/ServiceTest.php
+++ src/SimplyTestable/WebClientBundle/Tests/Functional/Services/MailChimp/ListRecipients/ServiceTest.php
@@ -5,22 +5,13 @@ namespace SimplyTestable\WebClientBundle\Tests\Functional\Services\MailChimp\Lis
 use SimplyTestable\WebClientBundle\Tests\Functional\BaseSimplyTestableTestCase;
 
 abstract class ServiceTest extends BaseSimplyTestableTestCase {
-    
-    protected function setUp() {
-        parent::setUp();
-        $entities = $this->getMailChimpListRecipientsService()->getEntityRepository()->findAll();
-        
-        foreach ($entities as $entity) {            
-            $this->getMailChimpListRecipientsService()->removeList($entity->getListId());
-        }
-    }
-    
+
     /**
-     * 
+     *
      * @return \SimplyTestable\WebClientBundle\Services\MailChimp\ListRecipientsService
      */
     protected function getMailChimpListRecipientsService() {
         return $this->container->get('simplytestable.services.mailchimp.listRecipients');
-    }    
+    }
 
 }
