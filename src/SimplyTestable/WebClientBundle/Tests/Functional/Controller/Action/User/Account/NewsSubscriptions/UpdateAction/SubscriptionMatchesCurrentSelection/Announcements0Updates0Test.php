2827574ead530fedaace653c8984e489e3f87e8a
Jon Cram <jon@webignition.net>
2017-12-21 12:10:12 +0000

    Re-implement Action/User/Account/NewsSubscriptionsController functional tests

--- src/SimplyTestable/WebClientBundle/Tests/Functional/Controller/Action/User/Account/NewsSubscriptions/UpdateAction/SubscriptionMatchesCurrentSelection/Announcements0Updates0Test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SimplyTestable\WebClientBundle\Tests\Functional\Controller\Action\User\Account\NewsSubscriptions\UpdateAction\SubscriptionMatchesCurrentSelection;
-
-class Announcements0Updates0Test extends SubscriptionMatchesCurrentSelectionTest {
-
-}
-
-
--- src/SimplyTestable/WebClientBundle/Tests/Functional/Controller/Action/User/Account/NewsSubscriptions/UpdateAction/SubscriptionMatchesCurrentSelection/Announcements1Updates0Test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SimplyTestable\WebClientBundle\Tests\Functional\Controller\Action\User\Account\NewsSubscriptions\UpdateAction\SubscriptionMatchesCurrentSelection;
-
-class Announcements1Updates0Test extends SubscriptionMatchesCurrentSelectionTest {
-
-}
-
-
