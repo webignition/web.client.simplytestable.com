d877e3d473dcf6db600a42cb1a41132230367f9c
Jon Cram <jon@webignition.net>
2017-12-22 12:16:25 +0000

    Re-implement Controller/Action/SignUp/User/ConfirmController tests

--- src/SimplyTestable/WebClientBundle/Controller/Action/SignUp/User/ConfirmController.php
+++ src/SimplyTestable/WebClientBundle/Controller/Action/SignUp/User/ConfirmController.php
@@ -5,97 +5,140 @@ namespace SimplyTestable\WebClientBundle\Controller\Action\SignUp\User;
 use SimplyTestable\WebClientBundle\Controller\BaseController;
 use SimplyTestable\WebClientBundle\Exception\CoreApplicationAdminRequestException;
 use SimplyTestable\WebClientBundle\Exception\Postmark\Response\Exception as PostmarkResponseException;
+use SimplyTestable\WebClientBundle\Services\Mail\Service as MailService;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
-class ConfirmController extends BaseController {
+class ConfirmController extends BaseController
+{
+    const FLASH_BAG_TOKEN_RESEND_ERROR_KEY = 'token_resend_error';
+    const FLASH_BAG_TOKEN_RESEND_ERROR_MESSAGE_USER_INVALID = 'invalid-user';
+    const FLASH_BAG_TOKEN_RESEND_ERROR_MESSAGE_CORE_APP_ADMIN_CREDENTIALS_INVALID = 'core-app-invalid-credentials';
+    const FLASH_BAG_TOKEN_RESEND_ERROR_MESSAGE_CORE_APP_UNKNOWN = 'core-app-unknown-error';
+    const FLASH_BAG_TOKEN_RESEND_ERROR_MESSAGE_POSTMARK_NOT_ALLOWED_TO_SEND = 'postmark-not-allowed-to-send';
+    const FLASH_BAG_TOKEN_RESEND_ERROR_MESSAGE_POSTMARK_INACTIVE_RECIPIENT = 'postmark-inactive-recipient';
+    const FLASH_BAG_TOKEN_RESEND_ERROR_MESSAGE_POSTMARK_UNKNOWN = 'postmark-failure';
 
-    public function resendAction($email) {
+    const FLASH_BAG_TOKEN_RESEND_SUCCESS_KEY = 'token_resend_confirmation';
+    const FLASH_BAG_TOKEN_RESEND_SUCCESS_MESSAGE = 'sent';
+
+    public function resendAction($email)
+    {
+        $userService = $this->container->get('simplytestable.services.userservice');
+        $session = $this->container->get('session');
+        $mailService = $this->container->get('simplytestable.services.mail.service');
+
         $redirectResponse = $this->redirect($this->generateUrl(
             'view_user_signup_confirm_index',
-            ['email' => $email]
+            ['email' => $email],
+            UrlGeneratorInterface::ABSOLUTE_URL
         ));
 
         try {
-            if ($this->getUserService()->exists($email) === false) {
-                $this->get('session')->getFlashBag()->set('token_resend_error', 'invalid-user');
+            if (!$userService->exists($email)) {
+                $session->getFlashBag()->set(
+                    self::FLASH_BAG_TOKEN_RESEND_ERROR_KEY,
+                    self::FLASH_BAG_TOKEN_RESEND_ERROR_MESSAGE_USER_INVALID
+                );
+
                 return $redirectResponse;
             }
         } catch (CoreApplicationAdminRequestException $coreApplicationAdminRequestException) {
-            if ($coreApplicationAdminRequestException->isInvalidCredentialsException()) {
-                $this->get('session')->getFlashBag()->set('token_resend_error', 'core-app-invalid-credentials');
-                $this->sendInvalidAdminCredentialsNotification();
-            } else {
-                $this->get('session')->getFlashBag()->set('token_resend_error', 'core-app-unknown-error');
-            }
+            $session->getFlashBag()->set(
+                self::FLASH_BAG_TOKEN_RESEND_ERROR_KEY,
+                self::FLASH_BAG_TOKEN_RESEND_ERROR_MESSAGE_CORE_APP_ADMIN_CREDENTIALS_INVALID
+            );
+
+            $this->sendInvalidAdminCredentialsNotification($mailService);
 
             return $redirectResponse;
         }
 
-        $token = $this->getUserService()->getConfirmationToken($email);
+        $token = $userService->getConfirmationToken($email);
 
         try {
-            $this->sendConfirmationToken($email, $token);
-            $this->get('session')->getFlashBag()->set('token_resend_confirmation', 'sent');
-
-            return $redirectResponse;
+            $this->sendConfirmationToken($mailService, $email, $token);
         } catch (PostmarkResponseException $postmarkResponseException) {
             if ($postmarkResponseException->isNotAllowedToSendException()) {
-                $this->get('session')->getFlashBag()->set('token_resend_error', 'postmark-not-allowed-to-send');
+                $session->getFlashBag()->set(
+                    self::FLASH_BAG_TOKEN_RESEND_ERROR_KEY,
+                    self::FLASH_BAG_TOKEN_RESEND_ERROR_MESSAGE_POSTMARK_NOT_ALLOWED_TO_SEND
+                );
             } elseif ($postmarkResponseException->isInactiveRecipientException()) {
-                $this->get('session')->getFlashBag()->set('token_resend_error', 'postmark-inactive-recipient');
+                $session->getFlashBag()->set(
+                    self::FLASH_BAG_TOKEN_RESEND_ERROR_KEY,
+                    self::FLASH_BAG_TOKEN_RESEND_ERROR_MESSAGE_POSTMARK_INACTIVE_RECIPIENT
+                );
             } else {
-                $this->get('session')->getFlashBag()->set('token_resend_error', 'postmark-failure');
+                $session->getFlashBag()->set(
+                    self::FLASH_BAG_TOKEN_RESEND_ERROR_KEY,
+                    self::FLASH_BAG_TOKEN_RESEND_ERROR_MESSAGE_POSTMARK_UNKNOWN
+                );
             }
 
             return $redirectResponse;
         }
+
+        $session->getFlashBag()->set(
+            self::FLASH_BAG_TOKEN_RESEND_SUCCESS_KEY,
+            self::FLASH_BAG_TOKEN_RESEND_SUCCESS_MESSAGE
+        );
+
+        return $redirectResponse;
     }
 
-
     /**
-     *
+     * @param MailService $mailService
      * @param string $email
-     * @param  $token
+     * @param string $token
+     *
      * @throws PostmarkResponseException
+     * @throws \SimplyTestable\WebClientBundle\Exception\Mail\Configuration\Exception
      */
-    private function sendConfirmationToken($email, $token) {
-        $sender = $this->getMailService()->getConfiguration()->getSender('default');
-        $messageProperties = $this->getMailService()->getConfiguration()->getMessageProperties('user_creation_confirmation');
+    private function sendConfirmationToken(MailService $mailService, $email, $token)
+    {
+        $mailConfiguration = $mailService->getConfiguration();
 
-        $confirmationUrl = $this->generateUrl('view_user_signup_confirm_index', array(
+        $sender = $mailConfiguration->getSender('default');
+        $messageProperties = $mailConfiguration->getMessageProperties('user_creation_confirmation');
+
+        $confirmationUrl = $this->generateUrl(
+            'view_user_signup_confirm_index',
+            [
                 'email' => $email
-            ), true).'?token=' . $token;
+            ],
+            UrlGeneratorInterface::ABSOLUTE_URL
+        ) . '?token=' . $token;
 
-        $message = $this->getMailService()->getNewMessage();
+        $viewName = 'SimplyTestableWebClientBundle:Email:user-creation-confirmation.txt.twig';
+
+        $message = $mailService->getNewMessage();
         $message->setFrom($sender['email'], $sender['name']);
         $message->addTo($email);
         $message->setSubject($messageProperties['subject']);
-        $message->setTextMessage($this->renderView('SimplyTestableWebClientBundle:Email:user-creation-confirmation.txt.twig', array(
+        $message->setTextMessage($this->renderView($viewName, [
             'confirmation_url' => $confirmationUrl,
             'confirmation_code' => $token
-        )));
+        ]));
 
-        $this->getMailService()->getSender()->send($message);
+        $mailService->getSender()->send($message);
     }
 
+    /**
+     * @param MailService $mailService
+     *
+     * @throws PostmarkResponseException
+     * @throws \SimplyTestable\WebClientBundle\Exception\Mail\Configuration\Exception
+     */
+    private function sendInvalidAdminCredentialsNotification(MailService $mailService)
+    {
+        $sender = $mailService->getConfiguration()->getSender('default');
 
-    private function sendInvalidAdminCredentialsNotification() {
-        $sender = $this->getMailService()->getConfiguration()->getSender('default');
-
-        $message = $this->getMailService()->getNewMessage();
+        $message = $mailService->getNewMessage();
         $message->setFrom($sender['email'], $sender['name']);
         $message->addTo('jon@simplytestable.com');
         $message->setSubject('Invalid admin user credentials');
         $message->setTextMessage('Invalid admin user credentials exception raised when calling UserService::exists()');
 
-        $this->getMailService()->getSender()->send($message);
-    }
-
-
-    /**
-     *
-     * @return \SimplyTestable\WebClientBundle\Services\Mail\Service
-     */
-    private function getMailService() {
-        return $this->get('simplytestable.services.mail.service');
+        $mailService->getSender()->send($message);
     }
 }
