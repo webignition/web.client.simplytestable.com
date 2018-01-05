8d68897ff158e5f5b3469c9ee336defbfae05217
Jon Cram <jon@webignition.net>
2018-01-05 15:12:25 +0000

    Test Services/CouponService

--- src/SimplyTestable/WebClientBundle/Services/CouponService.php
+++ src/SimplyTestable/WebClientBundle/Services/CouponService.php
@@ -5,8 +5,8 @@ use SimplyTestable\WebClientBundle\Model\Coupon;
 
 use Symfony\Component\HttpFoundation\Request as Request;
 
-class CouponService {
-
+class CouponService
+{
     const COUPON_COOKIE_NAME = 'simplytestable-coupon-code';
 
     /**
@@ -14,19 +14,24 @@ class CouponService {
      */
     private $request;
 
-
+    /**
+     * @var array
+     */
     private $couponData = [];
 
-
-    public function setCouponData($couponData) {
+    /**
+     * @param $couponData
+     */
+    public function setCouponData($couponData)
+    {
         $this->couponData = $couponData;
     }
-
 
     /**
      * @return null|Coupon
      */
-    public function get() {
+    public function get()
+    {
         if (!$this->has()) {
             return null;
         }
@@ -35,85 +40,79 @@ class CouponService {
         $coupon->setCode($this->getRequestCouponCode());
         $coupon->setIntro($this->getIntro());
         $coupon->setPercentOff($this->getPercentOff());
-
-        if ($this->isActive()) {
-            $coupon->setActive();
-        }
+        $coupon->setIsActive($this->isActive());
 
         return $coupon;
     }
 
-
     /**
      * @return bool
      */
-    private function isActive() {
-        return $this->getProperty('active', false, function ($value) {
-            return is_bool($value);
-        });
+    private function isActive()
+    {
+        return $this->getProperty('active', false);
     }
-
 
     /**
      * @return string
      */
-    private function getIntro() {
+    private function getIntro()
+    {
         return $this->getProperty('intro', '');
     }
 
-
     /**
      * @return string
      */
-    private function getPercentOff() {
-        return $this->getProperty('percent_off', 0, function ($value) {
-            return is_int($value);
-        });
+    private function getPercentOff()
+    {
+        return $this->getProperty('percent_off', 0);
     }
 
+    /**
+     * @param string $name
+     * @param mixed $default
+     *
+     * @return mixed
+     */
+    private function getProperty($name, $default = null)
+    {
+        $couponData = $this->couponData[$this->getRequestCouponCode()];
 
-    private function getProperty($name, $default = null, $validateCallback = null) {
-        if (!$this->has()) {
+        if (!isset($couponData[$name])) {
             return $default;
         }
 
-        $codeData = $this->couponData[$this->getRequestCouponCode()];
-
-        if (!isset($codeData[$name])) {
-            return $default;
-        }
-
-        if ($validateCallback instanceof \Closure) {
-            if ($validateCallback($codeData[$name]) === false) {
-                return $default;
-            }
-        }
-
-        return $codeData[$name];
+        return $couponData[$name];
     }
-
 
     /**
      * @return bool
      */
-    public function has() {
-        return !is_null($this->getRequestCouponCode());
-    }
+    public function has()
+    {
+        $couponCode = $this->getRequestCouponCode();
 
+        if (empty($couponCode)) {
+            return false;
+        }
+
+        return isset($this->couponData[$couponCode]);
+    }
 
     /**
      * @param Request $request
      */
-    public function setRequest(Request $request = null) {
+    public function setRequest(Request $request = null)
+    {
         $this->request = $request;
     }
-
 
     /**
      * @return string|null
      */
-    private function getRequestCouponCode() {
+    private function getRequestCouponCode()
+    {
         return $this->request->cookies->get(self::COUPON_COOKIE_NAME);
     }
-
 }
