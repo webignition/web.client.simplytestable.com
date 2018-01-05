4ae48eab202062e160eb9ab3edae1bbc19fcab11
Jon Cram <webignition@gmail.com>
2018-01-05 15:37:36 +0000

    Test Services/CouponService (#26)

--- src/SimplyTestable/WebClientBundle/Model/Coupon.php
+++ src/SimplyTestable/WebClientBundle/Model/Coupon.php
@@ -1,130 +1,118 @@
 <?php
 namespace SimplyTestable\WebClientBundle\Model;
 
-class Coupon {
-
+class Coupon implements \JsonSerializable
+{
     /**
      * @var string
      */
     private $code = '';
-
 
     /**
      * @var bool
      */
     private $isActive = false;
 
-
     /**
      * @var string
      */
     private $intro = '';
-
 
     /**
      * @var int
      */
     private $percentOff = 0;
 
-
     /**
      * @param string $code
-     * @return Coupon
      */
-    public function setCode($code) {
+    public function setCode($code)
+    {
         $this->code = $code;
-        return $this;
     }
-
 
     /**
      * @return string
      */
-    public function getCode() {
+    public function getCode()
+    {
         return $this->code;
     }
 
-
     /**
-     * @return Coupon
+     * @param bool $isActive
      */
-    public function setActive() {
-        $this->isActive = true;
-        return $this;
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
     }
-
-
-    /**
-     * @return Coupon
-     */
-    public function setInactive() {
-        $this->isActive = false;
-        return $this;
-    }
-
 
     /**
      * @return bool
      */
-    public function isActive() {
+    public function isActive()
+    {
         return $this->isActive;
     }
 
-
     /**
      * @param string $intro
-     * @return Coupon
      */
-    public function setIntro($intro) {
+    public function setIntro($intro)
+    {
         $this->intro = $intro;
-        return $this;
     }
-
 
     /**
      * @return string
      */
-    public function getIntro() {
+    public function getIntro()
+    {
         return $this->intro;
     }
 
-
     /**
      * @param $percentOff
-     * @return Coupon
      */
-    public function setPercentOff($percentOff) {
+    public function setPercentOff($percentOff)
+    {
         $this->percentOff = $percentOff;
-        return $this;
     }
-
 
     /**
      * @return int
      */
-    public function getPercentOff() {
+    public function getPercentOff()
+    {
         return $this->percentOff;
     }
-
 
     /**
      * @return float
      */
-    public function getPriceModifier() {
+    public function getPriceModifier()
+    {
         return 1 - ($this->getPercentOff() / 100);
     }
-
 
     /**
      * @return string
      */
-    public function __toString() {
-        return json_encode([
+    public function __toString()
+    {
+        return json_encode($this->jsonSerialize());
+    }
+
+    /**
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        return [
             'code' => $this->getCode(),
-            'active' => $this->isActive,
+            'active' => $this->isActive(),
             'intro' => $this->getIntro(),
             'percent_off' => $this->getPercentOff()
-        ]);
+        ];
     }
-    
 }
