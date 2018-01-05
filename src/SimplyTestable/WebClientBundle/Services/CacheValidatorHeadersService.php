da634029a021e457db6e19afbf1f02a0c245b69c
Jon Cram <webignition@gmail.com>
2018-01-05 13:30:10 +0000

    Test Services/CacheValidatorHeadersService (#24)

--- src/SimplyTestable/WebClientBundle/Services/CacheValidatorHeadersService.php
+++ src/SimplyTestable/WebClientBundle/Services/CacheValidatorHeadersService.php
@@ -1,110 +1,86 @@
 <?php
 namespace SimplyTestable\WebClientBundle\Services;
 
-use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
 use SimplyTestable\WebClientBundle\Entity\CacheValidatorHeaders;
 
-
-class CacheValidatorHeadersService {    
-    
-    const ENTITY_NAME = 'SimplyTestable\WebClientBundle\Entity\CacheValidatorHeaders';      
-    
+class CacheValidatorHeadersService
+{
     /**
-     *
-     * @var EntityManager 
+     * @var EntityManagerInterface
      */
-    private $entityManager;    
-    
+    private $entityManager;
 
     /**
-     *
-     * @var \Doctrine\ORM\EntityRepository
+     * @var EntityRepository
      */
-    private $entityRepository;    
+    private $entityRepository;
 
-    
     /**
-     *
-     * @param EntityManager $entityManager 
+     * @param EntityManagerInterface $entityManager
      */
-    public function __construct(EntityManager $entityManager) {
+    public function __construct(EntityManagerInterface $entityManager)
+    {
         $this->entityManager = $entityManager;
+        $this->entityRepository = $this->entityManager->getRepository(CacheValidatorHeaders::class);
     }
-    
-    
+
     /**
-     *
      * @param string $identifier
-     * @return \SimplyTestable\WebClientBundle\Entity\CacheValidatorHeaders 
+     *
+     * @return CacheValidatorHeaders
      */
-    public function get($identifier) {        
+    public function get($identifier)
+    {
         $cacheValidatorHeader = $this->fetch($identifier);
         if (is_null($cacheValidatorHeader)) {
             $cacheValidatorHeader = $this->create($identifier);
         }
-        
+
         return $cacheValidatorHeader;
     }
-    
-    
-    /**
-     *
-     * @param CacheValidatorHeaders $cacheValidatorHeaders 
-     */
-    public function store(CacheValidatorHeaders $cacheValidatorHeaders) {
-        $this->entityManager->persist($cacheValidatorHeaders);
-        $this->entityManager->flush();
-    }
-    
-    public function clear() {
-        $all = $this->getEntityRepository()->findAll();
+
+    public function clear()
+    {
+        $all = $this->entityRepository->findAll();
+
         foreach ($all as $cacheValidatorHeaders) {
             $this->entityManager->remove($cacheValidatorHeaders);
         }
-        
+
         $this->entityManager->flush();
-    }    
-    
-    
-    /**
-     *
-     * @param string $identifier
-     * @return \SimplyTestable\WebClientBundle\Entity\CacheValidatorHeaders 
-     */    
-    private function fetch($identifier) {
-        return $this->getEntityRepository()->findOneBy(array(
-            'identifier' => $identifier
-        ));        
     }
-    
-    
+
     /**
-     *
      * @param string $identifier
-     * @return \SimplyTestable\WebClientBundle\Entity\CacheValidatorHeaders 
+     *
+     * @return CacheValidatorHeaders
      */
-    private function create($identifier) {
+    private function fetch($identifier)
+    {
+        /* @var CacheValidatorHeaders $cacheValidatorHeaders */
+        $cacheValidatorHeaders = $this->entityRepository->findOneBy([
+            'identifier' => $identifier
+        ]);
+
+        return $cacheValidatorHeaders;
+    }
+
+    /**
+     * @param string $identifier
+     *
+     * @return CacheValidatorHeaders
+     */
+    private function create($identifier)
+    {
         $cacheValidatorHeaders = new CacheValidatorHeaders();
         $cacheValidatorHeaders->setIdentifier($identifier);
         $cacheValidatorHeaders->setLastModifiedDate(new \DateTime());
-        
+
         $this->entityManager->persist($cacheValidatorHeaders);
         $this->entityManager->flush();
-        
+
         return $cacheValidatorHeaders;
     }
-    
-    
-    /**
-     *
-     * @return \Doctrine\ORM\EntityRepository
-     */
-    public function getEntityRepository() {
-        if (is_null($this->entityRepository)) {
-            $this->entityRepository = $this->entityManager->getRepository(self::ENTITY_NAME);
-        }
-        
-        return $this->entityRepository;
-    }
-    
 }
