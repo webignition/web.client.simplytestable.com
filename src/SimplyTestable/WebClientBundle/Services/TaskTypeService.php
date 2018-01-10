0f2547d780ac3821051dae98abe100d148426e74
Jon Cram <webignition@gmail.com>
2018-01-10 18:39:36 +0000

    Test Services/TaskTypeService (#36)

--- src/SimplyTestable/WebClientBundle/Services/TaskTypeService.php
+++ src/SimplyTestable/WebClientBundle/Services/TaskTypeService.php
@@ -3,70 +3,74 @@ namespace SimplyTestable\WebClientBundle\Services;
 
 use SimplyTestable\WebClientBundle\Model\User;
 
-class TaskTypeService {
-
+class TaskTypeService
+{
     const ACCESS_LEVEL_PUBLIC = 'public';
     const ACCESS_LEVEL_AUTHENTICATED = 'authenticated';
     const ACCESS_LEVEL_EARLY_ACCESS = 'early-access';
 
+    /**
+     * @var array
+     */
     private $taskTypes;
+
+    /**
+     * @var string[]
+     */
     private $earlyAccessUsers;
 
     /**
-     *
      * @var User
      */
     private $user;
-
 
     /**
      * @var bool
      */
     private $isUserAuthenticated = false;
 
-
     /**
      * @param array $taskTypes
      */
-    public function setTaskTypes($taskTypes) {
+    public function setTaskTypes($taskTypes)
+    {
         $this->taskTypes = $taskTypes;
     }
-
 
     /**
      * @param string[] $earlyAccessUsers
      */
-    public function setEarlyAccessUsers($earlyAccessUsers) {
+    public function setEarlyAccessUsers($earlyAccessUsers)
+    {
         $this->earlyAccessUsers = $earlyAccessUsers;
     }
 
-
     /**
-     *
-     * @param \SimplyTestable\WebClientBundle\Model\User $user
+     * @param User $user
      */
-    public function setUser(User $user) {
+    public function setUser(User $user)
+    {
         $this->user = $user;
     }
 
-
-    public function setUserIsAuthenticated() {
+    public function setUserIsAuthenticated()
+    {
         $this->isUserAuthenticated = true;
     }
 
-
     /**
      * @return array
      */
-    public function get() {
+    public function get()
+    {
         return $this->taskTypes;
     }
 
-
     /**
      * @return array
      */
-    public function getAvailable() {
+    public function getAvailable()
+    {
         $taskTypes = [];
 
         foreach ($this->taskTypes as $taskTypeKey => $taskTypeDetails) {
@@ -78,58 +82,28 @@ class TaskTypeService {
         return $taskTypes;
     }
 
-
     /**
      * @param $taskTypeDetails
-     * @return bool
-     */
-    private function isAllowedTaskType($taskTypeDetails) {
-        if ($this->isPublicAccessTaskType($taskTypeDetails)) {
-            return true;
-        }
-
-        if ($this->isAuthenticatedAccessTaskType($taskTypeDetails) && $this->isUserAuthenticated) {
-            return true;
-        }
-
-        if ($this->isEarlyAccessTaskType($taskTypeDetails) && $this->isEarlyAccessUser()) {
-            return true;
-        }
-    }
-
-
-    /**
-     * @param $taskTypeDetails
-     * @return bool
-     */
-    private function isPublicAccessTaskType($taskTypeDetails) {
-        return $taskTypeDetails['access-level'] === self::ACCESS_LEVEL_PUBLIC;
-    }
-
-    /**
-     * @param $taskTypeDetails
-     * @return bool
-     */
-    private function isAuthenticatedAccessTaskType($taskTypeDetails) {
-        return $taskTypeDetails['access-level'] === self::ACCESS_LEVEL_AUTHENTICATED;
-    }
-
-
-    /**
-     * @param $taskTypeDetails
-     * @return bool
-     */
-    private function isEarlyAccessTaskType($taskTypeDetails) {
-        return $taskTypeDetails['access-level'] === self::ACCESS_LEVEL_EARLY_ACCESS;
-    }
-
-
-    /**
      *
-     * @return boolean
+     * @return bool
      */
-    private function isEarlyAccessUser() {
-        return in_array($this->user->getUsername(), $this->earlyAccessUsers);
-    }
+    private function isAllowedTaskType($taskTypeDetails)
+    {
+        $isPublicAccessTaskType = $taskTypeDetails['access-level'] === self::ACCESS_LEVEL_PUBLIC;
+        $isAuthenticatedAccessTaskType = $taskTypeDetails['access-level'] === self::ACCESS_LEVEL_AUTHENTICATED;
+        $isEarlyAccessTaskType = $taskTypeDetails['access-level'] === self::ACCESS_LEVEL_EARLY_ACCESS;
+        $isEarlyAccessUser = in_array($this->user->getUsername(), $this->earlyAccessUsers);
 
+        if ($isPublicAccessTaskType) {
+            return true;
+        }
+
+        if ($isAuthenticatedAccessTaskType && $this->isUserAuthenticated) {
+            return true;
+        }
+
+        if ($isEarlyAccessTaskType && $isEarlyAccessUser) {
+            return true;
+        }
+    }
 }
