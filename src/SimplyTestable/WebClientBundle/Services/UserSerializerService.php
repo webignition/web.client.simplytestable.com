3782575185c2b38f50b1e2f4e3c1076b957304b5
Jon Cram <jon@webignition.net>
2018-01-16 18:03:18 +0000

    Test Services/UserSerializerService

--- src/SimplyTestable/WebClientBundle/Services/UserSerializerService.php
+++ src/SimplyTestable/WebClientBundle/Services/UserSerializerService.php
@@ -3,173 +3,154 @@ namespace SimplyTestable\WebClientBundle\Services;
 
 use SimplyTestable\WebClientBundle\Model\User;
 
-class UserSerializerService {    
-    
+class UserSerializerService
+{
+    const SERIALIZED_USER_USERNAME_KEY = 'username';
+    const SERIALIZED_USER_PASSWORD_KEY = 'password';
+    const SERIALIZED_USER_KEY_KEY = 'key';
+    const SERIALIZED_USER_IV_KEY = 'iv';
+
     /**
-     *
      * @var string
      */
-    private $surrogateKey;  
-    
-    
+    private $surrogateKey;
+
     /**
-     *
      * @var string
      */
     private $iv;
-    
-    
+
     /**
-     *
      * @var string
      */
     private $key;
-    
-    
+
     /**
-     * 
      * @param string $key
      */
-    public function __construct($key) {
+    public function __construct($key)
+    {
         $this->key = md5($key);
-    }    
-    
-    
-    public function serialize(User $user) {        
-        return array(
-            'username' => $this->encrypt($user->getUsername(), $this->getSurrogateKey()),
-            'password' => $this->encrypt($user->getPassword(), $this->getSurrogateKey()),
-            'key' => $this->encrypt($this->getSurrogateKey(), $this->key),
-            'iv' => $this->getIv(),
-        );       
+        $this->surrogateKey = md5(rand());
+
+        $isSize = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
+        $this->iv = mcrypt_create_iv($isSize, MCRYPT_RAND);
     }
-    
-    
+
     /**
-     * 
-     * @param \SimplyTestable\WebClientBundle\Model\User $user
+     * @param User $user
+     *
+     * @return array
+     */
+    public function serialize(User $user)
+    {
+        return array(
+            self::SERIALIZED_USER_USERNAME_KEY => $this->encrypt($user->getUsername(), $this->surrogateKey),
+            self::SERIALIZED_USER_PASSWORD_KEY => $this->encrypt($user->getPassword(), $this->surrogateKey),
+            self::SERIALIZED_USER_KEY_KEY => $this->encrypt($this->surrogateKey, $this->key),
+            self::SERIALIZED_USER_IV_KEY => $this->iv,
+        );
+    }
+
+    /**
+     * @param User $user
+     *
      * @return string
      */
-    public function serializeToString(User $user) {
+    public function serializeToString(User $user)
+    {
         $serializedUser = $this->serialize($user);
-        
+
         foreach ($serializedUser as $key => $value) {
             $serializedUser[$key] = base64_encode($value);
         }
-        
+
         return base64_encode(json_encode($serializedUser));
     }
-    
-    
+
     /**
-     * 
      * @param string $user
-     * @return \SimplyTestable\WebClientBundle\Model\User
+     *
+     * @return User
      */
-    public function unserializedFromString($user) {        
-        $base64EncodedUserValues = json_decode(base64_decode($user), true);        
+    public function unserializedFromString($user)
+    {
+        $base64EncodedUserValues = json_decode(base64_decode($user), true);
         if (!is_array($base64EncodedUserValues)) {
             return null;
         }
-        
-        if (!count($base64EncodedUserValues)) {
+
+        if (empty($base64EncodedUserValues)) {
             return null;
         }
-        
-        $expectedKeys = array('username', 'password', 'key', 'iv');
-        foreach  ($expectedKeys as $expectedKey) {
+
+        $expectedKeys = [
+            self::SERIALIZED_USER_USERNAME_KEY,
+            self::SERIALIZED_USER_PASSWORD_KEY,
+            self::SERIALIZED_USER_KEY_KEY,
+            self::SERIALIZED_USER_IV_KEY,
+        ];
+
+        foreach ($expectedKeys as $expectedKey) {
             if (!isset($base64EncodedUserValues[$expectedKey])) {
                 return null;
             }
         }
-        
-        $userValues = array();
-        
-        foreach ($base64EncodedUserValues as $key => $value) {            
+
+        $userValues = [];
+
+        foreach ($base64EncodedUserValues as $key => $value) {
             $base64DecodedValue = base64_decode($value);
             if ($base64DecodedValue == '') {
                 return null;
             }
-            
+
             $userValues[$key] = $base64DecodedValue;
         }
-        
-        return $this->unserialize($userValues);        
+
+        return $this->unserialize($userValues);
     }
-    
-    
-    
+
     /**
-     * 
      * @param array $serializedUser
-     * @return \SimplyTestable\WebClientBundle\Model\User
+     *
+     * @return User
      */
-    public function unserialize($serializedUser) {
-        $this->iv = $serializedUser['iv'];
-        $this->surrogateKey = $this->decrypt($serializedUser['key'], $this->key);
-        
+    public function unserialize($serializedUser)
+    {
+        $this->iv = $serializedUser[self::SERIALIZED_USER_IV_KEY];
+        $this->surrogateKey = $this->decrypt($serializedUser[self::SERIALIZED_USER_KEY_KEY], $this->key);
+
         $user = new User();
-        $user->setUsername(trim($this->decrypt($serializedUser['username'], $this->getSurrogateKey())));
-        $user->setPassword(trim($this->decrypt($serializedUser['password'], $this->getSurrogateKey())));
-        
+        $user->setUsername(
+            trim($this->decrypt($serializedUser[self::SERIALIZED_USER_USERNAME_KEY], $this->surrogateKey))
+        );
+        $user->setPassword(
+            trim($this->decrypt($serializedUser[self::SERIALIZED_USER_PASSWORD_KEY], $this->surrogateKey))
+        );
+
         return $user;
     }
-    
-    
+
     /**
-     * 
-     * @return string
-     */
-    private function getSurrogateKey() {
-        if (is_null($this->surrogateKey)) {
-            $this->surrogateKey = md5(rand());
-        }
-        
-        return $this->surrogateKey;
-    }
-    
-    
-    /**
-     * 
      * @param string $plaintext
      * @param string $key
+     *
      * @return string
      */
-    private function encrypt($plaintext, $key) {        
-        return mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $plaintext, MCRYPT_MODE_ECB, $this->getIv());        
+    private function encrypt($plaintext, $key)
+    {
+        return mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $plaintext, MCRYPT_MODE_ECB, $this->iv);
     }
-    
-    
+
     /**
-     * 
      * @param string $ciphertext
      * @param string $key
-     * @return strings
-     */
-    private function decrypt($ciphertext, $key) {        
-        return mcrypt_decrypt( MCRYPT_RIJNDAEL_256, $key, $ciphertext, MCRYPT_MODE_ECB, $this->getIv());
-    }
-    
-    
-    /**
-     * 
-     * @return int
-     */
-    private function getIvSize() {
-        return mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
-    }
-    
-    
-    /**
-     * 
+     *
      * @return string
      */
-    private function getIv() {
-        if (is_null($this->iv)) {
-            $this->iv = mcrypt_create_iv($this->getIvSize(), MCRYPT_RAND);
-        }
-        
-        return $this->iv;
+    private function decrypt($ciphertext, $key)
+    {
+        return mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $ciphertext, MCRYPT_MODE_ECB, $this->iv);
     }
-    
 }
