eb6bd71fe4ef6c8951412c8e900618f66b170b2e
Jon Cram <jon@webignition.net>
2018-01-17 11:22:00 +0000

    Test Controller/MailChimp/EventController

--- src/SimplyTestable/WebClientBundle/Controller/MailChimp/EventController.php
+++ src/SimplyTestable/WebClientBundle/Controller/MailChimp/EventController.php
@@ -2,114 +2,72 @@
 
 namespace SimplyTestable\WebClientBundle\Controller\MailChimp;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use SimplyTestable\WebClientBundle\Controller\BaseController;
 use SimplyTestable\WebClientBundle\Event\MailChimp\Event as MailChimpEvent;
 
-class EventController extends BaseController {    
-    
+class EventController extends BaseController
+{
     const LISTENER_EVENT_PREFIX = 'mailchimp.';
-    
+
     /**
+     * @param Request $request
      *
-     * @var MailChimpEvent
+     * @return Response
      */
-    private $event = null;
-    
-    public function indexAction() {
-        if ($this->get('request')->getMethod() == 'GET') {
-            return new Response('', 200);
+    public function indexAction(Request $request)
+    {
+        $dispatcher = $this->container->get('event_dispatcher');
+        $logger = $this->container->get('logger');
+
+        if ($request->isMethod('GET')) {
+            return new Response();
         }
-        
-        if (!$this->get('request')->request->has('type')) {
+
+        $requestData = $request->request;
+
+        if (!$requestData->has('type')) {
             return new Response('', 400);
         }
-        
-        if (!$this->dispatcherHasListenerForEvent()) {
+
+        $event = new MailChimpEvent($requestData);
+        $listenerEventName = self::LISTENER_EVENT_PREFIX . $event->getType();
+        $dispatcherHasListener = count($dispatcher->getListeners($listenerEventName)) > 0;
+
+        if (!$dispatcherHasListener) {
             return new Response('', 400);
         }
-        
+
         try {
-            $this->getDispatcher()->dispatch(
-                    $this->getListenerEventName(),
-                    $this->getEvent()
-            );            
+            $dispatcher->dispatch(
+                $listenerEventName,
+                $event
+            );
         } catch (\DomainException $domainException) {
             // Invalid event data list_id
-            if ($domainException->getCode() === 2) {
-                $eventData = $this->get('request')->request->get('data');
-                
-                $logMessage = 'Controller\MailChimp\EventController ';
-                
-                $logMessage .= (array_key_exists('list_id', $eventData))
-                    ? 'invalid list_id "' . $eventData['list_id'] . '" in event data'
-                    : 'no list_id in event data';
-                
-                $this->getLogger()->error('Controller\MailChimp\EventController \DomainException START');
-                $this->getLogger()->error($logMessage);
-                $this->getLogger()->error($domainException);
-                $this->getLogger()->error($domainException);                
-                $this->getLogger()->error('Controller\MailChimp\EventController \DomainException END');
-            } else {
-                throw $domainException;
-            }
+            $eventData = $requestData->get('data');
 
+            $logMessage = 'Controller\MailChimp\EventController ';
+
+            $logMessage .= (array_key_exists('list_id', $eventData))
+                ? 'invalid list_id "' . $eventData['list_id'] . '" in event data'
+                : 'no list_id in event data';
+
+            $logger->error('Controller\MailChimp\EventController \DomainException START');
+            $logger->error($logMessage);
+            $logger->error($domainException);
+            $logger->error($domainException);
+            $logger->error('Controller\MailChimp\EventController \DomainException END');
         } catch (\UnexpectedValueException $unexpectedValueException) {
             // Event raw data is missing "data"
-            if ($unexpectedValueException->getCode() === 2) {
-                $this->getLogger()->error('Controller\MailChimp\EventController \UnexpectedValueException START');                
-                $this->getLogger()->error('Controller\MailChimp\EventController event raw data is missing "data"');
-                $this->getLogger()->error($this->get('request'));
-                $this->getLogger()->error($unexpectedValueException);                
-                $this->getLogger()->error('Controller\MailChimp\EventController \UnexpectedValueException END');                
-                
-
-            } else {
-                throw $unexpectedValueException;
-            }
+            $logger->error('Controller\MailChimp\EventController \UnexpectedValueException START');
+            $logger->error('Controller\MailChimp\EventController event raw data is missing "data"');
+            $logger->error($request);
+            $logger->error($unexpectedValueException);
+            $logger->error('Controller\MailChimp\EventController \UnexpectedValueException END');
         }
-        
+
         return new Response('', 200);
     }
-    
-    
-    /**
-     * 
-     * @return \SimplyTestable\WebClientBundle\Event\MailChimp\Event
-     */
-    private function getEvent() {
-        if (is_null($this->event)) {
-            $this->event = new MailChimpEvent($this->get('request')->request);
-        }
-        
-        return $this->event;
-    }
-    
-    
-    /**
-     * 
-     * @return boolean
-     */
-    private function dispatcherHasListenerForEvent() {        
-        return count($this->getDispatcher()->getListeners($this->getListenerEventName())) > 0;      
-    }
-    
-    
-    /**
-     * 
-     * @return \Symfony\Component\EventDispatcher\ContainerAwareEventDispatcher
-     */
-    private function getDispatcher() {
-        return $this->get('event_dispatcher');
-    }
-    
-    
-    /**
-     * 
-     * @return string
-     */
-    private function getListenerEventName() {
-        return self::LISTENER_EVENT_PREFIX . $this->getEvent()->getType();
-    }
-    
 }
