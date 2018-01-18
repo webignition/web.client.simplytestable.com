89d543b90955de20039aac3c9da83425e53df05d
Jon Cram <webignition@gmail.com>
2018-01-18 18:47:33 +0000

    Test Controller/View/Test/History/WebsiteListController (#57)

--- src/SimplyTestable/WebClientBundle/Controller/View/Test/History/WebsiteListController.php
+++ src/SimplyTestable/WebClientBundle/Controller/View/Test/History/WebsiteListController.php
@@ -4,11 +4,24 @@ namespace SimplyTestable\WebClientBundle\Controller\View\Test\History;
 
 use SimplyTestable\WebClientBundle\Controller\View\Test\ViewController;
 use SimplyTestable\WebClientBundle\Interfaces\Controller\RequiresValidUser;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
-class WebsiteListController extends ViewController implements RequiresValidUser {
+class WebsiteListController extends ViewController implements RequiresValidUser
+{
+    /**
+     * @return JsonResponse
+     */
+    public function indexAction()
+    {
+        $remoteTestService = $this->container->get('simplytestable.services.remotetestservice');
+        $userService = $this->container->get('simplytestable.services.userservice');
+        $cacheableResponseService = $this->container->get('simplytestable.services.cacheableresponseservice');
 
-    public function indexAction() {
-        return $this->renderUncacheableResponse($this->getTestService()->getRemoteTestService()->getFinishedWebsites());
+        $user = $userService->getUser();
+
+        $remoteTestService->setUser($user);
+        $finishedWebsites = $remoteTestService->getFinishedWebsites();
+
+        return $cacheableResponseService->getUncacheableResponse(new JsonResponse($finishedWebsites));
     }
-
 }
