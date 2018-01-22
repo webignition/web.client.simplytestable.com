8182b23516ae6b2eae3dcc7f12937d3b6b1f10d0
Jon Cram <jon@webignition.net>
2018-01-22 16:12:58 +0000

    Test Controller/View/Test/ResultsPartial/FinishedSummary/IndexController

--- src/SimplyTestable/WebClientBundle/Controller/View/Test/Results/Partial/FinishedSummary/IndexController.php
+++ src/SimplyTestable/WebClientBundle/Controller/View/Test/Results/Partial/FinishedSummary/IndexController.php
@@ -2,38 +2,70 @@
 
 namespace SimplyTestable\WebClientBundle\Controller\View\Test\Results\Partial\FinishedSummary;
 
-use SimplyTestable\WebClientBundle\Controller\View\Test\CacheableViewController;
+use SimplyTestable\WebClientBundle\Controller\BaseViewController;
+use SimplyTestable\WebClientBundle\Exception\WebResourceException;
 use SimplyTestable\WebClientBundle\Interfaces\Controller\RequiresValidUser;
 use SimplyTestable\WebClientBundle\Interfaces\Controller\Test\RequiresValidOwner;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class IndexController extends CacheableViewController implements RequiresValidUser, RequiresValidOwner
-{    
-    protected function modifyViewName($viewName) {
-        return str_replace(array(
-            ':Test'
-        ), array(
-            ':bs3/Test'
-        ), $viewName);
-    }
-    
-    public function indexAction($website, $test_id) {
-        $test = $this->getTestService()->get($website, $test_id);
-        
-        $viewData = array(
-            'test' => array(
+class IndexController extends BaseViewController implements RequiresValidUser, RequiresValidOwner
+{
+    /**
+     * @param Request $request
+     * @param string $website
+     * @param int $test_id
+     *
+     * @return Response
+     *
+     * @throws WebResourceException
+     */
+    public function indexAction(Request $request, $website, $test_id)
+    {
+        $cacheValidatorService = $this->container->get('simplytestable.services.cachevalidator');
+        $templating = $this->container->get('templating');
+        $testService = $this->container->get('simplytestable.services.testservice');
+        $remoteTestService = $this->container->get('simplytestable.services.remotetestservice');
+        $userService = $this->container->get('simplytestable.services.userservice');
+
+        $user = $userService->getUser();
+        $remoteTestService->setUser($user);
+
+        $response = $cacheValidatorService->createResponse($request, [
+            'website' => $website,
+            'test_id' => $test_id,
+        ]);
+
+        if ($cacheValidatorService->isNotModified($response)) {
+            return $response;
+        }
+
+        $test = $testService->get($website, $test_id);
+        $remoteTest = $remoteTestService->get();
+
+        $viewData = [
+            'test' => [
                 'test' => $test,
-                'remote_test' => $this->getTestService()->getRemoteTestService()->get(),
-            )
+                'remote_test' => $remoteTest,
+            ]
+        ];
+
+        $content = $templating->render(
+            'SimplyTestableWebClientBundle:bs3/Test/Results/Partial/FinishedSummary/Index:index.html.twig',
+            array_merge($this->getDefaultViewParameters(), $viewData)
         );
 
-        return $this->renderCacheableResponse($viewData);
+        $response->setContent($content);
+        $response->headers->set('content-type', 'text/html');
+
+        return $response;
     }
 
-    public function getCacheValidatorParameters() {
-        return array(
-            'test_id' => $this->getRequest()->attributes->get('test_id'),
-            'website' => $this->getRequest()->attributes->get('website')
-        );
+    /**
+     * {@inheritdoc}
+     */
+    public function getInvalidOwnerResponse()
+    {
+        return new Response('', 400);
     }
-
 }
