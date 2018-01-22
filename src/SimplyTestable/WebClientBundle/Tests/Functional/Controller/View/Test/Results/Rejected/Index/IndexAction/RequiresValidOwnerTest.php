3a5c8d54270aa3a1e1782fbfc48e84e5ece7a747
Jon Cram <webignition@gmail.com>
2018-01-22 18:16:23 +0000

    Test Controller/View/Test/Results/Rejected/IndexController (#65)

--- src/SimplyTestable/WebClientBundle/Tests/Functional/Controller/View/Test/Results/Rejected/Index/IndexAction/RequiresValidOwnerTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace SimplyTestable\WebClientBundle\Tests\Functional\Controller\View\Test\Results\Rejected\Index\IndexAction;
-
-use SimplyTestable\WebClientBundle\Tests\Functional\Controller\Base\RequiresValidOwnerTest as BaseRequiresValidOwnerTest;
-
-class RequiresValidOwnerTest extends BaseRequiresValidOwnerTest {
-}
