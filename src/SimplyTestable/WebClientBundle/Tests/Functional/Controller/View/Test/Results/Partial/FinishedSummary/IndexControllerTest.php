117151bf6fcce2812c1526ddd0f28d852454d483
Jon Cram <webignition@gmail.com>
2018-01-22 16:20:36 +0000

    Test Controller/View/Test/ResultsPartial/FinishedSummary/IndexController (#63)

---
+++ src/SimplyTestable/WebClientBundle/Tests/Functional/Controller/View/Test/Results/Partial/FinishedSummary/IndexControllerTest.php
@@ -0,0 +1,263 @@
+<?php
+
+namespace SimplyTestable\WebClientBundle\Tests\Functional\Controller\View\Test\Results\Partial\FinishedSummary;
+
+use SimplyTestable\WebClientBundle\Controller\View\Test\Results\Partial\FinishedSummary\IndexController;
+use SimplyTestable\WebClientBundle\Entity\Test\Test;
+use SimplyTestable\WebClientBundle\Exception\WebResourceException;
+use SimplyTestable\WebClientBundle\Model\RemoteTest\RemoteTest;
+use SimplyTestable\WebClientBundle\Model\User;
+use SimplyTestable\WebClientBundle\Services\UserService;
+use SimplyTestable\WebClientBundle\Tests\Factory\ContainerFactory;
+use SimplyTestable\WebClientBundle\Tests\Factory\HttpHistory;
+use SimplyTestable\WebClientBundle\Tests\Factory\HttpResponseFactory;
+use SimplyTestable\WebClientBundle\Tests\Factory\MockFactory;
+use SimplyTestable\WebClientBundle\Tests\Functional\BaseSimplyTestableTestCase;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IndexControllerTest extends BaseSimplyTestableTestCase
+{
+    const VIEW_NAME = 'SimplyTestableWebClientBundle:bs3/Test/Results/Partial/FinishedSummary/Index:index.html.twig';
+    const ROUTE_NAME = 'view_test_results_partial_finishedsummary_index';
+
+    const WEBSITE = 'http://example.com/';
+    const TEST_ID = 1;
+    const USER_EMAIL = 'user@example.com';
+
+    /**
+     * @var IndexController
+     */
+    private $indexController;
+
+    /**
+     * @var array
+     */
+    private $remoteTestData = [
+        'id' => self::TEST_ID,
+        'website' => self::WEBSITE,
+        'task_types' => [],
+        'user' => self::USER_EMAIL,
+        'state' => Test::STATE_FAILED_NO_SITEMAP,
+        'task_type_options' => [],
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->indexController = new IndexController();
+    }
+
+    public function testIndexActionInvalidUserGetRequest()
+    {
+        $this->setHttpFixtures([
+            HttpResponseFactory::create(404),
+        ]);
+
+        $router = $this->container->get('router');
+        $requestUrl = $router->generate(self::ROUTE_NAME, [
+            'website' => self::WEBSITE,
+            'test_id' => self::TEST_ID,
+        ]);
+
+        $this->client->request(
+            'GET',
+            $requestUrl
+        );
+
+        /* @var RedirectResponse $response */
+        $response = $this->client->getResponse();
+
+        $this->assertTrue($response->isRedirect('http://localhost/signout/'));
+    }
+
+    public function testIndexActionInvalidOwnerGetRequest()
+    {
+        $this->setHttpFixtures([
+            HttpResponseFactory::create(200),
+            HttpResponseFactory::createForbiddenResponse(),
+        ]);
+
+        $router = $this->container->get('router');
+        $requestUrl = $router->generate(self::ROUTE_NAME, [
+            'website' => self::WEBSITE,
+            'test_id' => self::TEST_ID,
+        ]);
+
+        $this->client->request(
+            'GET',
+            $requestUrl
+        );
+
+        /* @var RedirectResponse $response */
+        $response = $this->client->getResponse();
+
+        $this->assertTrue($response->isClientError());
+        $this->assertEmpty($response->getContent());
+    }
+
+    public function testIndexActionPublicUserGetRequest()
+    {
+        $this->setHttpFixtures([
+            HttpResponseFactory::create(200),
+            HttpResponseFactory::createJsonResponse($this->remoteTestData),
+        ]);
+
+        $router = $this->container->get('router');
+        $requestUrl = $router->generate(self::ROUTE_NAME, [
+            'website' => self::WEBSITE,
+            'test_id' => self::TEST_ID,
+        ]);
+
+        $this->client->request(
+            'GET',
+            $requestUrl
+        );
+
+        /* @var Response $response */
+        $response = $this->client->getResponse();
+        $this->assertTrue($response->isSuccessful());
+    }
+
+    /**
+     * @dataProvider indexActionRenderDataProvider
+     *
+     * @param EngineInterface $templatingEngine
+     *
+     * @throws WebResourceException
+     */
+    public function testIndexActionRender(EngineInterface $templatingEngine)
+    {
+        $this->setHttpFixtures([
+            HttpResponseFactory::createJsonResponse($this->remoteTestData),
+        ]);
+
+        $containerFactory = new ContainerFactory($this->container);
+        $container = $containerFactory->create(
+            [
+//                'router',
+                'simplytestable.services.testservice',
+                'simplytestable.services.remotetestservice',
+                'simplytestable.services.userservice',
+                'simplytestable.services.cachevalidator',
+//                'simplytestable.services.urlviewvalues',
+            ],
+            [
+                'templating' => $templatingEngine,
+            ],
+            [
+                'public_site',
+                'external_links',
+            ]
+        );
+
+        $this->indexController->setContainer($container);
+
+        $response = $this->indexController->indexAction(new Request(), self::WEBSITE, self::TEST_ID);
+        $this->assertInstanceOf(Response::class, $response);
+    }
+
+    /**
+     * @return array
+     */
+    public function indexActionRenderDataProvider()
+    {
+        return [
+            'default' => [
+                'templatingEngine' => MockFactory::createTemplatingEngine([
+                    'render' => [
+                        'withArgs' => function ($viewName, $parameters) {
+                            $this->assertEquals(self::VIEW_NAME, $viewName);
+                            $this->assertViewParameterKeys($parameters);
+
+                            $testData = $parameters['test'];
+                            $this->assertInternalType('array', $testData);
+
+                            $this->assertEquals(
+                                [
+                                    'test',
+                                    'remote_test',
+                                ],
+                                array_keys($testData)
+                            );
+
+                            /* @var Test $test */
+                            $test = $testData['test'];
+                            $this->assertInstanceOf(Test::class, $test);
+                            $this->assertEquals(self::TEST_ID, $test->getTestId());
+                            $this->assertEquals(self::WEBSITE, (string)$test->getWebsite());
+
+                            /* @var RemoteTest $remoteTest */
+                            $remoteTest = $testData['remote_test'];
+                            $this->assertInstanceOf(RemoteTest::class, $remoteTest);
+                            $this->assertEquals(self::TEST_ID, $remoteTest->getId());
+                            $this->assertEquals(self::WEBSITE, $remoteTest->getWebsite());
+
+                            return true;
+                        },
+                        'return' => new Response(),
+                    ],
+                ]),
+            ],
+        ];
+    }
+
+    public function testIndexActionCachedResponse()
+    {
+        $this->setHttpFixtures([
+            HttpResponseFactory::createJsonResponse($this->remoteTestData),
+        ]);
+
+        $request = new Request();
+
+        $this->container->set('request', $request);
+        $this->indexController->setContainer($this->container);
+
+        $response = $this->indexController->indexAction($request, self::WEBSITE, self::TEST_ID);
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $responseLastModified = new \DateTime($response->headers->get('last-modified'));
+        $responseLastModified->modify('+1 hour');
+
+        $newRequest = $request->duplicate();
+
+        $newRequest->headers->set('if-modified-since', $responseLastModified->format('c'));
+        $newResponse = $this->indexController->indexAction($newRequest, self::WEBSITE, self::TEST_ID);
+
+        $this->assertInstanceOf(Response::class, $newResponse);
+        $this->assertEquals(304, $newResponse->getStatusCode());
+    }
+
+    /**
+     * @param array $parameters
+     */
+    private function assertViewParameterKeys(array $parameters)
+    {
+        $this->assertEquals(
+            [
+                'user',
+                'is_logged_in',
+                'public_site',
+                'external_links',
+                'test',
+            ],
+            array_keys($parameters)
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function tearDown()
+    {
+        \Mockery::close();
+        parent::tearDown();
+    }
+}
