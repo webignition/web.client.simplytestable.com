9854ce0be52a0152273dd7f5926f480e453c038a
Jon Cram <webignition@gmail.com>
2018-01-23 13:24:56 +0000

    Test Controller/View/Test/Results/PreparingStatsController (#66)

--- src/SimplyTestable/WebClientBundle/Controller/View/Test/Results/PreparingStatsController.php
+++ src/SimplyTestable/WebClientBundle/Controller/View/Test/Results/PreparingStatsController.php
@@ -3,48 +3,81 @@
 namespace SimplyTestable\WebClientBundle\Controller\View\Test\Results;
 
 use SimplyTestable\WebClientBundle\Controller\View\Test\ViewController;
+use SimplyTestable\WebClientBundle\Exception\WebResourceException;
 use SimplyTestable\WebClientBundle\Interfaces\Controller\RequiresValidUser;
 use SimplyTestable\WebClientBundle\Interfaces\Controller\Test\RequiresValidOwner;
-use SimplyTestable\WebClientBundle\Entity\Test\Test;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
-class PreparingStatsController extends ViewController implements RequiresValidUser, RequiresValidOwner {
-
-    public function getInvalidOwnerResponse() {
-        return $this->renderUncacheableResponse(array(
-            'id' => 0,
-            'completion_percent' => 0,
-            'remaining_tasks_to_retrieve_count' => 0,
-            'local_task_count' => 0,
-            'remote_task_count' => 0
-        ));
+class PreparingStatsController extends ViewController implements RequiresValidUser, RequiresValidOwner
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getInvalidOwnerResponse()
+    {
+        return $this->createJsonResponse(0, 0, 0, 0, 0);
     }
 
-    protected function modifyViewName($viewName) {
-        return str_replace(array(
-            ':Test',
-            'verbose.html.twig'
-        ), array(
-            ':bs3/Test',
-            'index.html.twig'
-        ), $viewName);
+    /**
+     * @param string $website
+     * @param int $test_id
+     *
+     * @return JsonResponse
+     *
+     * @throws WebResourceException
+     */
+    public function indexAction($website, $test_id)
+    {
+        $testService = $this->container->get('simplytestable.services.testservice');
+        $remoteTestService = $this->container->get('simplytestable.services.remotetestservice');
+        $userService = $this->container->get('simplytestable.services.userservice');
+
+        $user = $userService->getUser();
+        $remoteTestService->setUser($user);
+
+        $test = $testService->get($website, $test_id);
+        $remoteTest = $remoteTestService->get();
+
+        $localTaskCount = $test->getTaskCount();
+        $remoteTaskCount = $remoteTest->getTaskCount();
+
+        $completionPercent = 0 === $remoteTaskCount
+            ? 100
+            : round(($localTaskCount / $remoteTaskCount) * 100);
+
+        $remainingTasksToRetrieveCount = $remoteTaskCount - $localTaskCount;
+
+        return $this->createJsonResponse(
+            $test_id,
+            $completionPercent,
+            $remainingTasksToRetrieveCount,
+            $localTaskCount,
+            $remoteTaskCount
+        );
     }
 
-
-    public function indexAction($website, $test_id) {
-        $remoteTest = $this->getRemoteTest();
-
-        $localTaskCount = $this->getTest()->getTaskCount();
-        $completionPercent = round(($localTaskCount / $remoteTest->getTaskCount()) * 100);
-        $remainingTasksToRetrieveCount = $remoteTest->getTaskCount() - $localTaskCount;
-
-        return $this->renderUncacheableResponse([
-            'id' => $this->getTest()->getTestId(),
+    /**
+     * @param int $testId
+     * @param int $completionPercent
+     * @param int $remainingTasksToRetrieveCount
+     * @param int $localTaskCount
+     * @param int $remoteTaskCount
+     *
+     * @return JsonResponse
+     */
+    private function createJsonResponse(
+        $testId,
+        $completionPercent,
+        $remainingTasksToRetrieveCount,
+        $localTaskCount,
+        $remoteTaskCount
+    ) {
+        return new JsonResponse([
+            'id' => $testId,
             'completion_percent' => $completionPercent,
             'remaining_tasks_to_retrieve_count' => $remainingTasksToRetrieveCount,
             'local_task_count' => $localTaskCount,
-            'remote_task_count' => $remoteTest->getTaskCount()
+            'remote_task_count' => $remoteTaskCount
         ]);
     }
-
 }
