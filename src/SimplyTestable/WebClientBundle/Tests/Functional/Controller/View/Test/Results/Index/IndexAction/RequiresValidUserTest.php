4c53c29d85427be6de92e7102fe34b576d7a61e5
Jon Cram <jon@webignition.net>
2018-01-24 13:18:21 +0000

    Remove obsolete tests

--- src/SimplyTestable/WebClientBundle/Tests/Functional/Controller/View/Test/Results/Index/IndexAction/RequiresValidUserTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace SimplyTestable\WebClientBundle\Tests\Functional\Controller\View\Test\Results\Index\IndexAction;
-
-use SimplyTestable\WebClientBundle\Tests\Functional\Controller\Base\RequiresValidUserTest as BaseRequiresValidUserTest;
-
-class RequiresValidUserTest extends BaseRequiresValidUserTest {
-}
