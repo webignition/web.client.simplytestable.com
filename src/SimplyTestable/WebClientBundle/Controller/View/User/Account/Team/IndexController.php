5ad5e698cdc3d6d262963d6945c38783d073cf1f
Jon Cram <webignition@gmail.com>
2018-01-25 17:27:54 +0000

    Test Controller/View/User/Account/Team/IndexController (#78)

--- src/SimplyTestable/WebClientBundle/Controller/View/User/Account/Team/IndexController.php
+++ src/SimplyTestable/WebClientBundle/Controller/View/User/Account/Team/IndexController.php
@@ -2,20 +2,18 @@
 
 namespace SimplyTestable\WebClientBundle\Controller\View\User\Account\Team;
 
+use SimplyTestable\WebClientBundle\Controller\Action\User\Account\TeamController;
 use SimplyTestable\WebClientBundle\Controller\BaseViewController;
+use SimplyTestable\WebClientBundle\Exception\WebResourceException;
 use SimplyTestable\WebClientBundle\Interfaces\Controller\RequiresPrivateUser;
 use SimplyTestable\WebClientBundle\Interfaces\Controller\IEFiltered;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 
-class IndexController extends BaseViewController implements RequiresPrivateUser, IEFiltered {
-
+class IndexController extends BaseViewController implements RequiresPrivateUser, IEFiltered
+{
     const STRIPE_CARD_CHECK_KEY_POSTFIX = '_check';
-
-    protected function modifyViewName($viewName) {
-        return str_replace(':User', ':bs3/User', $viewName);
-    }
 
     /**
      * {@inheritdoc}
@@ -27,68 +25,66 @@ class IndexController extends BaseViewController implements RequiresPrivateUser,
         ], true));
     }
 
-    public function indexAction() {
-        $this->getUserService()->setUser($this->getUser());
-        $userSummary = $this->getUserService()->getSummary();
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     *
+     * @throws \Exception
+     * @throws WebResourceException
+     */
+    public function indexAction(Request $request)
+    {
+        $userService = $this->container->get('simplytestable.services.userservice');
+        $session = $this->get('session');
+        $teamService = $this->container->get('simplytestable.services.teamservice');
+        $teamInviteService = $this->container->get('simplytestable.services.teaminviteservice');
+        $templating = $this->container->get('templating');
+
+        $user = $userService->getUser();
+        $userSummary = $userService->getSummary();
+        $flashBag = $session->getFlashBag();
+        $teamSummary = $userSummary->getTeamSummary();
+
+        $teamCreateErrorValues = $flashBag->get(TeamController::FLASH_BAG_CREATE_ERROR_KEY);
+        $teamCreateError = empty($teamCreateErrorValues) ? '' : $teamCreateErrorValues[0];
 
         $viewData = [
-            'plan_presentation_name' => $this->getPlanPresentationName($userSummary->getPlan()->getAccountPlan()->getName()),
+            'plan_presentation_name' => ucwords(
+                $userSummary->getPlan()->getAccountPlan()->getName()
+            ),
             'user_summary' => $userSummary,
-            'team_create_error' => $this->getFlash('team_create_error', true),
+            'team_create_error' => $teamCreateError,
         ];
 
-        $teamInviteGetData = $this->get('session')->getFlashBag()->get('team_invite_get');
+        $teamInviteGetData = $flashBag->get(TeamController::FLASH_BAG_TEAM_INVITE_GET_KEY);
         if (is_array($teamInviteGetData) && count($teamInviteGetData)) {
             $viewData['team_invite_get'] = $teamInviteGetData;
         }
 
-        $teamInviteResendData = $this->get('session')->getFlashBag()->get('team_invite_resend');
+        $teamInviteResendData = $flashBag->get(TeamController::FLASH_BAG_TEAM_RESEND_INVITE_KEY);
         if (is_array($teamInviteResendData) && count($teamInviteResendData)) {
             $viewData['team_invite_resend'] = $teamInviteResendData;
         }
 
-        if ($userSummary->getTeamSummary()->isInTeam()) {
-            $team = $this->getTeamService()->getTeam();
+        if ($teamSummary->isInTeam()) {
+            $team = $teamService->getTeam();
             $viewData['team'] = $team;
 
-            if ($team->getLeader() == $this->getUser()->getUsername()) {
-                $viewData['invites'] = $this->getTeamInviteService()->getForTeam();
+            if ($team->getLeader() === $user->getUsername()) {
+                $viewData['invites'] = $teamInviteService->getForTeam();
             }
         }
 
-        if ($userSummary->getTeamSummary()->hasInvite()) {
-            $viewData['invites'] = $this->getTeaminviteService()->getForUser();
+        if ($teamSummary->hasInvite()) {
+            $viewData['invites'] = $teamInviteService->getForUser();
         }
 
-        return $this->renderResponse($this->getRequest(), $viewData);
+        $content = $templating->render(
+            'SimplyTestableWebClientBundle:bs3/User/Account/Team/Index:index.html.twig',
+            array_merge($this->getDefaultViewParameters(), $viewData)
+        );
+
+        return new Response($content);
     }
-
-
-    /**
-     *
-     * @param string $plan
-     * @return string
-     */
-    private function getPlanPresentationName($plan) {
-        return ucwords($plan);
-    }
-
-
-    /**
-     *
-     * @return \SimplyTestable\WebClientBundle\Services\TeamInviteService
-     */
-    private function getTeamInviteService() {
-        return $this->container->get('simplytestable.services.teaminviteservice');
-    }
-
-
-    /**
-     *
-     * @return \SimplyTestable\WebClientBundle\Services\TeamService
-     */
-    private function getTeamService() {
-        return $this->container->get('simplytestable.services.teamservice');
-    }
-
 }
