ba8b9b7085495971f5e8c022b9f584139c3eba3b
Jon Cram <jon@webignition.net>
2018-01-26 16:42:26 +0000

    Create FlashBagValues service

---
+++ src/SimplyTestable/WebClientBundle/Services/FlashBagValues.php
@@ -0,0 +1,42 @@
+<?php
+namespace SimplyTestable\WebClientBundle\Services;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class FlashBagValues
+{
+    /**
+     * @var Session
+     */
+    private $session;
+
+    /**
+     * @param Session $session
+     */
+    public function __construct(Session $session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * @param string[] $keys
+     *
+     * @return array
+     */
+    public function get($keys)
+    {
+        $values = [];
+
+        $flashBag = $this->session->getFlashBag();
+
+        foreach ($keys as $key) {
+            $flashValues = $flashBag->get($key);
+
+            if (!empty($flashValues)) {
+                $values[$key] = $flashValues[0];
+            }
+        }
+
+        return $values;
+    }
+}
