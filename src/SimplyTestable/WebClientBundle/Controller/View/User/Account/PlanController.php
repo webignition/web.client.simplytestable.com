e7c72014678f87af5b92cd63606a20c8184852f3
Jon Cram <webignition@gmail.com>
2018-01-26 15:14:27 +0000

    Test Controller/View/User/Account/PlanController (#81)

--- src/SimplyTestable/WebClientBundle/Controller/View/User/Account/PlanController.php
+++ src/SimplyTestable/WebClientBundle/Controller/View/User/Account/PlanController.php
@@ -3,88 +3,105 @@
 namespace SimplyTestable\WebClientBundle\Controller\View\User\Account;
 
 use SimplyTestable\WebClientBundle\Controller\BaseViewController;
+use SimplyTestable\WebClientBundle\Exception\WebResourceException;
 use SimplyTestable\WebClientBundle\Interfaces\Controller\RequiresPrivateUser;
 use SimplyTestable\WebClientBundle\Interfaces\Controller\IEFiltered;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
-class PlanController extends BaseViewController implements RequiresPrivateUser, IEFiltered {
-
-    protected function modifyViewName($viewName) {
-        return str_replace(':User', ':bs3/User', $viewName);
-    }
-
+class PlanController extends BaseViewController implements RequiresPrivateUser, IEFiltered
+{
     /**
      * {@inheritdoc}
      */
     public function getUserSignInRedirectResponse(Request $request)
     {
-        return new RedirectResponse($this->generateUrl('view_user_signin_index', [
-            'redirect' => base64_encode(json_encode(['route' => 'view_user_account_card_index']))
-        ], true));
+        $router = $this->container->get('router');
+
+        $redirectUrl = $router->generate(
+            'view_user_signin_index',
+            [
+                'redirect' => base64_encode(json_encode(['route' => 'view_user_account_plan_index']))
+            ],
+            UrlGeneratorInterface::ABSOLUTE_URL
+        );
+
+        return new RedirectResponse($redirectUrl);
     }
 
-    public function indexAction() {
-        $userSummary = $this->getUserService()->getSummary($this->getUser());
+    /**
+     * @return RedirectResponse|Response
+     *
+     * @throws \Exception
+     * @throws WebResourceException
+     */
+    public function indexAction()
+    {
+        $userService = $this->container->get('simplytestable.services.userservice');
+        $teamService = $this->container->get('simplytestable.services.teamservice');
+        $templating = $this->container->get('templating');
+        $planService = $this->container->get('simplytestable.services.plansservice');
+        $router = $this->container->get('router');
+        $session = $this->container->get('session');
+
+        $user = $userService->getUser();
+        $userSummary = $userService->getSummary();
+        $team = null;
 
         if ($userSummary->getPlan()->getAccountPlan()->getIsCustom()) {
-            return new RedirectResponse($this->generateUrl('view_user_account_index_index', true));
+            $redirectUrl = $router->generate(
+                'view_user_account_index_index',
+                [],
+                UrlGeneratorInterface::ABSOLUTE_URL
+            );
+
+            return new RedirectResponse($redirectUrl);
         }
 
-        if ($userSummary->hasStripeCustomer() && $userSummary->getStripeCustomer()->hasDiscount()) {
-            $priceModifier = (100 - $userSummary->getStripeCustomer()->getDiscount()->getCoupon()->getPercentOff()) / 100;
-            $this->getPlansService()->setPriceModifier($priceModifier);
+        $stripeCustomer = $userSummary->getStripeCustomer();
+
+        if (!empty($stripeCustomer) && $stripeCustomer->hasDiscount()) {
+            $priceModifier = (100 - $stripeCustomer->getDiscount()->getCoupon()->getPercentOff()) / 100;
+            $planService->setPriceModifier($priceModifier);
         }
 
-        $viewData = array_merge([
-            'user_summary' => $userSummary,
-            'plan_presentation_name' => $this->getPlanPresentationName($userSummary->getPlan()->getAccountPlan()->getName()),
-            'plans' => $this->getPlansService()->listPremiumOnly()->getList(),
-            'currency_map' => $this->container->getParameter('currency_map')
-        ], $this->getViewFlashValues(array(
-            'plan_subscribe_error',
-            'plan_subscribe_success'
-        )));
+        $flashBag = $session->getFlashBag();
+        $flashBagValues = [
+            'plan_subscribe_error' => '',
+            'plan_subscribe_success' => '',
+        ];
+
+        foreach ($flashBagValues as $key => $currentValue) {
+            $values = $flashBag->get($key);
+            if (!empty($values)) {
+                $flashBagValues[$key] = $values[0];
+            }
+        }
+
+        $viewData = array_merge(
+            $this->getDefaultViewParameters(),
+            [
+                'user_summary' => $userSummary,
+                'plan_presentation_name' => ucwords($userSummary->getPlan()->getAccountPlan()->getName()),
+                'plans' => $planService->listPremiumOnly()->getList(),
+                'currency_map' => $this->container->getParameter('currency_map'),
+                'plan_subscribe_error' => $flashBagValues['plan_subscribe_error'],
+                'plan_subscribe_success' => $flashBagValues['plan_subscribe_success'],
+            ]
+        );
 
         if ($userSummary->getTeamSummary()->isInTeam()) {
-            $this->getTeamService()->setUser($this->getUser());
-            $viewData['team'] = $this->getTeamService()->getTeam();
+            $teamService->setUser($user);
+            $viewData['team'] = $teamService->getTeam();
         }
 
-        return $this->renderResponse($this->getRequest(), $viewData);
+        $content = $templating->render(
+            'SimplyTestableWebClientBundle:bs3/User/Account/Plan:index.html.twig',
+            $viewData
+        );
+
+        return new Response($content);
     }
-
-
-    /**
-     *
-     * @param string $plan
-     * @return string
-     */
-    private function getPlanPresentationName($plan) {
-        if (substr_count($plan, '-custom')) {
-            $planParts = explode('-custom', $plan);
-            return $planParts[0];
-        }
-
-        return ucwords($plan);
-    }
-
-
-    /**
-     *
-     * @return \SimplyTestable\WebClientBundle\Services\PlansService
-     */
-    private function getPlansService() {
-        return $this->container->get('simplytestable.services.plansService');
-    }
-
-
-    /**
-     *
-     * @return \SimplyTestable\WebClientBundle\Services\TeamService
-     */
-    private function getTeamService() {
-        return $this->container->get('simplytestable.services.teamservice');
-    }
-
 }
--- src/SimplyTestable/WebClientBundle/Tests/Functional/Controller/View/User/Account/Plan/Index/IndexAction/IEFilteredTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace SimplyTestable\WebClientBundle\Tests\Functional\Controller\View\User\Account\Plan\IndexAction;
-
-use SimplyTestable\WebClientBundle\Tests\Functional\Controller\Base\IEFilteredTest as BaseIEFilteredTest;
-
-class IEFilteredTest extends BaseIEFilteredTest {}
