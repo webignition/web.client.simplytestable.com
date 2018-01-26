b822fbf9210d20c817e33528a844141cf31d7032
Jon Cram <jon@webignition.net>
2018-01-26 11:36:21 +0000

    Remove obsolete tests

--- src/SimplyTestable/WebClientBundle/Tests/Functional/Controller/View/User/Account/Card/Index/IndexAction/IEFilteredTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace SimplyTestable\WebClientBundle\Tests\Functional\Controller\View\User\Account\Card\IndexAction;
-
-use SimplyTestable\WebClientBundle\Tests\Functional\Controller\Base\IEFilteredTest as BaseIEFilteredTest;
-
-class IEFilteredTest extends BaseIEFilteredTest {}
