d9b1c9621f1178ce2b941fdf1aca455b9c795f66
Jon Cram <webignition@gmail.com>
2018-01-29 16:53:04 +0000

    Test Controller/View/User/SignUp/IndexController (#88)

--- src/SimplyTestable/WebClientBundle/Tests/Functional/Controller/View/User/SignUp/Index/IndexAction/IEFilteredTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace SimplyTestable\WebClientBundle\Tests\Functional\Controller\View\User\SignUp\Index\IndexAction;
-
-use SimplyTestable\WebClientBundle\Tests\Functional\Controller\Base\IEFilteredTest as BaseIEFilteredTest;
-
-class IEFilteredTest extends BaseIEFilteredTest {}
