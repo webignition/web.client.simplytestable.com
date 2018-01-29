ed0e2b195e83660c634df3d0b4d633bfb648a497
Jon Cram <jon@webignition.net>
2018-01-29 11:07:36 +0000

    Test Controller/View/User/Account/ResetPassword/ChooseController

--- src/SimplyTestable/WebClientBundle/Controller/View/User/ResetPassword/ChooseController.php
+++ src/SimplyTestable/WebClientBundle/Controller/View/User/ResetPassword/ChooseController.php
@@ -2,44 +2,59 @@
 
 namespace SimplyTestable\WebClientBundle\Controller\View\User\ResetPassword;
 
+use SimplyTestable\WebClientBundle\Controller\Action\User\ResetPassword\IndexController;
+use SimplyTestable\WebClientBundle\Controller\BaseViewController;
+use SimplyTestable\WebClientBundle\Exception\CoreApplicationAdminRequestException;
 use SimplyTestable\WebClientBundle\Interfaces\Controller\IEFiltered;
-use SimplyTestable\WebClientBundle\Controller\View\CacheableViewController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class ChooseController extends CacheableViewController implements IEFiltered {
-    
-    protected function modifyViewName($viewName) {
-        return str_replace(':User', ':bs3/User', $viewName);
-    }
-    
-    public function indexAction($email, $token) {
-        return $this->renderCacheableResponse($this->getTransientViewData());
-    }
-    
-    
-    private function getTransientViewData($flush = true) {        
-        $email = trim($this->getRequest()->attributes->get('email'));
-        $token = trim($this->getRequest()->attributes->get('token'));
-        
-        $actualToken = $this->getUserService()->getConfirmationToken($email);
-        
-        $userResetPasswordError = $this->getFlash('user_reset_password_error', $flush);                
-        if ($userResetPasswordError == '' && $token != $actualToken) {
+class ChooseController extends BaseViewController implements IEFiltered
+{
+    /**
+     * @param Request $request
+     * @param string $email
+     * @param string $token
+     *
+     * @return Response
+     *
+     * @throws CoreApplicationAdminRequestException
+     */
+    public function indexAction(Request $request, $email, $token)
+    {
+        $cacheValidatorService = $this->container->get('simplytestable.services.cachevalidator');
+        $userService = $this->container->get('simplytestable.services.userservice');
+        $flashBagValuesService = $this->container->get('simplytestable.services.flashbagvalues');
+        $templating = $this->container->get('templating');
+
+        $staySignedIn = $request->query->get('stay-signed-in');
+        $actualToken = $userService->getConfirmationToken($email);
+        $userResetPasswordError = $flashBagValuesService->getSingle(IndexController::FLASH_BAG_REQUEST_ERROR_KEY);
+
+        if ($token !== $actualToken) {
             $userResetPasswordError = 'invalid-token';
-        }        
-        
-        $viewData = array(
+        }
+
+        $viewData = [
             'email' => $email,
             'token' => $token,
-            'stay_signed_in' => $this->getRequest()->query->get('stay-signed-in'),
-            'user_reset_password_error' => $userResetPasswordError
-        );        
-     
-        return $viewData;
-    }
+            'stay_signed_in' => $staySignedIn,
+            'user_reset_password_error' => $userResetPasswordError,
+        ];
 
-    
-    public function getCacheValidatorParameters() {        
-        return $this->getTransientViewData(false);
-    }
+        $response = $cacheValidatorService->createResponse($request, $viewData);
 
+        if ($cacheValidatorService->isNotModified($response)) {
+            return $response;
+        }
+
+        $content = $templating->render(
+            'SimplyTestableWebClientBundle:bs3/User/ResetPassword/Choose:index.html.twig',
+            array_merge($this->getDefaultViewParameters(), $viewData)
+        );
+
+        $response->setContent($content);
+
+        return $response;
+    }
 }
