c8031837b8d290cba6dd99cb95f144fa5d22c527
Jon Cram <jon@webignition.net>
2018-01-31 18:59:04 +0000

    Upgrade to Symfony 2.4

--- src/SimplyTestable/WebClientBundle/Factory/AssetFactory.php
+++ src/SimplyTestable/WebClientBundle/Factory/AssetFactory.php
@@ -6,13 +6,24 @@ use Assetic\Factory\Worker\CacheBustingWorker;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
-use Assetic\Factory\LazyAssetManager;
 
 class AssetFactory extends BaseAssetFactory
 {
-    public function __construct(KernelInterface $kernel, ContainerInterface $container, ParameterBagInterface $parameterBag, $baseDir, $debug = false)
-    {
+    /**
+     * @param KernelInterface $kernel
+     * @param ContainerInterface $container
+     * @param ParameterBagInterface $parameterBag
+     * @param $baseDir
+     * @param bool $debug
+     */
+    public function __construct(
+        KernelInterface $kernel,
+        ContainerInterface $container,
+        ParameterBagInterface $parameterBag,
+        $baseDir,
+        $debug = false
+    ) {
         parent::__construct($kernel, $container, $parameterBag, $baseDir, $debug);
-        $this->addWorker(new CacheBustingWorker(new LazyAssetManager($this)));
+        $this->addWorker(new CacheBustingWorker());
     }
 }
