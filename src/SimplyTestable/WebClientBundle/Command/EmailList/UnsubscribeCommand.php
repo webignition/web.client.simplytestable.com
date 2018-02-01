f90d390035c69188660280b71bd4279282dcf330
Jon Cram <jon@webignition.net>
2018-02-01 12:41:19 +0000

    Define simplytestable:emaillist:unsubscribe as a service

--- src/SimplyTestable/WebClientBundle/Command/EmailList/UnsubscribeCommand.php
+++ src/SimplyTestable/WebClientBundle/Command/EmailList/UnsubscribeCommand.php
@@ -1,27 +1,57 @@
 <?php
 namespace SimplyTestable\WebClientBundle\Command\EmailList;
 
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use SimplyTestable\WebClientBundle\Services\MailChimp\Service as MailChimpService;
 
-class UnsubscribeCommand extends EmailListCommand
+class UnsubscribeCommand extends Command
 {
-    
+    const NAME = 'simplytestable:emaillist:unsubscribe';
+    const ARG_LIST_ID = 'listId';
+    const ARG_EMAIL = 'email';
+
+    /**
+     * @var MailChimpService
+     */
+    private $mailChimpService;
+
+    /**
+     * @param MailChimpService $mailChimpService
+     * @param string|null $name
+     */
+    public function __construct(
+        MailChimpService $mailChimpService,
+        $name = null
+    ) {
+        parent::__construct($name);
+
+        $this->mailChimpService = $mailChimpService;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     protected function configure()
     {
         $this
-            ->setName('simplytestable:emaillist:unsubscribe')
+            ->setName(self::NAME)
             ->setDescription('Unsubscribe a user from a email list')
-            ->addArgument('listId', InputArgument::REQUIRED, 'id of list to unsubscribe from')                
-            ->addArgument('email', InputArgument::REQUIRED, 'email of user to unsubscribe')
+            ->addArgument(self::ARG_LIST_ID, InputArgument::REQUIRED, 'id of list to unsubscribe from')
+            ->addArgument(self::ARG_EMAIL, InputArgument::REQUIRED, 'email of user to unsubscribe')
         ;
     }
- 
 
-    protected function execute(InputInterface $input, OutputInterface $output) {     
-        $this->getMailchimpService()->unsubscribe($input->getArgument('listId'), $input->getArgument('email'));
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->mailChimpService->unsubscribe(
+            $input->getArgument(self::ARG_LIST_ID),
+            $input->getArgument(self::ARG_EMAIL)
+        );
     }
- 
 }
