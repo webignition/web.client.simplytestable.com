5c9115ae46094bc2ebb1d152bb1c4d1ec27a97dc
Jon Cram <jon@webignition.net>
2018-02-01 10:05:14 +0000

    Update Stripe/EventController

--- src/SimplyTestable/WebClientBundle/Controller/Stripe/EventController.php
+++ src/SimplyTestable/WebClientBundle/Controller/Stripe/EventController.php
@@ -3,6 +3,7 @@
 namespace SimplyTestable\WebClientBundle\Controller\Stripe;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use SimplyTestable\WebClientBundle\Event\Stripe\Event as StripeEvent;
 
@@ -11,13 +12,15 @@ class EventController extends Controller
     const LISTENER_EVENT_PREFIX = 'stripe.';
 
     /**
+     * @param Request $request
+     *
      * @return Response
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $dispatcher = $this->container->get('event_dispatcher');
 
-        $event = new StripeEvent($this->get('request')->request);
+        $event = new StripeEvent($request->request);
         $listenerEventName = self::LISTENER_EVENT_PREFIX . $event->getName();
 
         if (!$event->hasUser()) {
