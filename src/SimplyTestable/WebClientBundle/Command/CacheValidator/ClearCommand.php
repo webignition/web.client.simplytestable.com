6ff0b628ca078f37b992ba0afc369fe7352a51f9
Jon Cram <webignition@gmail.com>
2018-02-01 11:17:40 +0000

    Register simplytestable:cachevalidator:clear as service (#112)

--- src/SimplyTestable/WebClientBundle/Command/CacheValidator/ClearCommand.php
+++ src/SimplyTestable/WebClientBundle/Command/CacheValidator/ClearCommand.php
@@ -1,25 +1,49 @@
 <?php
 namespace SimplyTestable\WebClientBundle\Command\CacheValidator;
 
+use SimplyTestable\WebClientBundle\Services\CacheValidatorHeadersService;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 use SimplyTestable\WebClientBundle\Command\BaseCommand;
 
 class ClearCommand extends BaseCommand
-{ 
-    
+{
+    const NAME = 'simplytestable:cachevalidator:clear';
+
+    /**
+     * @var CacheValidatorHeadersService
+     */
+    private $cacheValidatorHeadersService;
+
+    /**
+     * @param CacheValidatorHeadersService $cacheValidatorHeadersService
+     * @param string|null $name
+     */
+    public function __construct(CacheValidatorHeadersService $cacheValidatorHeadersService, $name = null)
+    {
+        parent::__construct($name);
+
+        $this->cacheValidatorHeadersService = $cacheValidatorHeadersService;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     protected function configure()
     {
         $this
-            ->setName('simplytestable:cachevalidator:clear')
+            ->setName(self::NAME)
             ->setDescription('Clear cache validator headers')
         ;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('Clearing cache validator headers');
-        $this->getContainer()->get('simplytestable.services.cachevalidatorheadersservice')->clear();
-    }   
+        $this->cacheValidatorHeadersService->clear();
+    }
 }
