754bffd5764d1295d83f8478397bf79c8abc06f9
Jon Cram <jon@webignition.net>
2018-02-01 11:58:52 +0000

    Register simplytestable:emaillist:retrieve-recipients as service

--- src/SimplyTestable/WebClientBundle/Command/EmailList/RetrieveRecipientsCommand.php
+++ src/SimplyTestable/WebClientBundle/Command/EmailList/RetrieveRecipientsCommand.php
@@ -2,19 +2,59 @@
 namespace SimplyTestable\WebClientBundle\Command\EmailList;
 
 use Doctrine\ORM\EntityManagerInterface;
+use SimplyTestable\WebClientBundle\Services\MailChimp\ListRecipientsService;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use SimplyTestable\WebClientBundle\Services\MailChimp\Service as MailChimpService;
 
-class RetrieveRecipientsCommand extends EmailListCommand
+class RetrieveRecipientsCommand extends Command
 {
+    const NAME = 'simplytestable:emaillist:retrieve-recipients';
+    const ARG_LIST_NAME = 'listName';
+
+    /**
+     * @var ListRecipientsService
+     */
+    private $listRecipientsService;
+
+    /**
+     * @var MailChimpService
+     */
+    private $mailChimpService;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * @param ListRecipientsService $listRecipientsService
+     * @param MailChimpService $mailChimpService
+     * @param EntityManagerInterface $entityManager
+     * @param string|null $name
+     */
+    public function __construct(
+        ListRecipientsService $listRecipientsService,
+        MailChimpService $mailChimpService,
+        EntityManagerInterface $entityManager,
+        $name = null
+    ) {
+        parent::__construct($name);
+
+        $this->listRecipientsService = $listRecipientsService;
+        $this->mailChimpService = $mailChimpService;
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * {@inheritdoc}
      */
     protected function configure()
     {
         $this
-            ->setName('simplytestable:emaillist:retrieve-recipients')
+            ->setName(self::NAME)
             ->setDescription('Retrieve recipients for a given list')
             ->addArgument('listName', InputArgument::REQUIRED, 'name of list to retrieve')
         ;
@@ -25,23 +65,18 @@ class RetrieveRecipientsCommand extends EmailListCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $mailChimpListRecipientsService = $this->getContainer()->get(
-            'simplytestable.services.mailchimp.listrecipients'
-        );
+        $listName = $input->getArgument(self::ARG_LIST_NAME);
 
-        /* @var EntityManagerInterface $entityManager */
-        $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
-
-        if (!($mailChimpListRecipientsService->hasListIdentifier($input->getArgument('listName')))) {
+        if (!($this->listRecipientsService->hasListIdentifier($listName))) {
             return 0;
         }
 
-        $output->write('Getting recipients for "' . $input->getArgument('listName') . '" ... ');
+        $output->write('Getting recipients for "' . $listName . '" ... ');
 
-        $listRecipients = $mailChimpListRecipientsService->get($input->getArgument('listName'));
-        $listRecipients->setRecipients(array());
+        $listRecipients = $this->listRecipientsService->get($listName);
+        $listRecipients->setRecipients([]);
 
-        $members = $this->getMailchimpService()->retrieveMembers($input->getArgument('listName'));
+        $members = $this->mailChimpService->retrieveMembers($listName);
 
         $output->writeln(count($members) . ' recipients retrieved');
 
@@ -49,8 +84,8 @@ class RetrieveRecipientsCommand extends EmailListCommand
             $listRecipients->addRecipient($member['email']);
         }
 
-        $entityManager->persist($listRecipients);
-        $entityManager->flush();
+        $this->entityManager->persist($listRecipients);
+        $this->entityManager->flush();
 
         return 0;
     }
