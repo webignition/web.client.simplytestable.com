809478871bbf219b1279b01d76fdafaf733bbd6d
Jon Cram <jon@webignition.net>
2018-02-13 19:06:08 +0000

    Update test

--- src/SimplyTestable/WebClientBundle/Tests/Functional/Command/EmailList/UnsubscribeCommandTest.php
+++ src/SimplyTestable/WebClientBundle/Tests/Functional/Command/EmailList/UnsubscribeCommandTest.php
@@ -5,6 +5,7 @@ namespace SimplyTestable\WebClientBundle\Tests\Functional\Command\EmailList;
 use Guzzle\Plugin\Mock\MockPlugin;
 use SimplyTestable\WebClientBundle\Command\EmailList\SubscribeCommand;
 use SimplyTestable\WebClientBundle\Command\EmailList\UnsubscribeCommand;
+use SimplyTestable\WebClientBundle\Services\MailChimp\Client;
 use SimplyTestable\WebClientBundle\Tests\Factory\HttpResponseFactory;
 use SimplyTestable\WebClientBundle\Tests\Functional\AbstractBaseTestCase;
 use Symfony\Component\Console\Input\ArrayInput;
@@ -29,8 +30,8 @@ class UnsubscribeCommandTest extends AbstractBaseTestCase
 
     public function testRun()
     {
-        $mailChimpClient = $this->container->get('simplytestable.services.mailchimp.client');
-        $mailChimpClient->addSubscriber(new MockPlugin([
+        $mailChimpClient = $this->container->get(Client::class);
+        $mailChimpClient->getHttpClient()->addSubscriber(new MockPlugin([
             HttpResponseFactory::createSuccessResponse(),
         ]));
 
