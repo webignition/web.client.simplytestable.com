5627c3c1ee94669b8011894a46f418a7f2ff63da
Jon Cram <jon@webignition.net>
2018-02-16 11:22:13 +0000

    Load task_types parameter from static config file

--- src/SimplyTestable/WebClientBundle/DependencyInjection/SimplyTestableWebClientExtension.php
+++ src/SimplyTestable/WebClientBundle/DependencyInjection/SimplyTestableWebClientExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -15,14 +16,35 @@ use Symfony\Component\DependencyInjection\Loader;
 class SimplyTestableWebClientExtension extends Extension
 {
     /**
+     * @var string[]
+     */
+    private $parameterFiles = [
+        'task_types.yml',
+    ];
+
+    /**
      * {@inheritDoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $fileLocator = new FileLocator([
+            __DIR__.'/../Resources/config',
+            __DIR__.'/../Resources/config/parameters',
+        ]);
+
+        $loader = new Loader\YamlFileLoader($container, $fileLocator);
         $loader->load('services.yml');
+
+        foreach ($this->parameterFiles as $parameterFile) {
+            $parameterName = str_replace('.yml', '', $parameterFile);
+
+            $container->setParameter(
+                $parameterName,
+                Yaml::parse(file_get_contents($fileLocator->locate($parameterFile)))
+            );
+        }
     }
 }
