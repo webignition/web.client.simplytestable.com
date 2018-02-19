d140aa921e69d73f296566ca165b92a26efde0b1
Jon Cram <jon@webignition.net>
2018-02-19 10:38:09 +0000

    Autoload AppKernel, AppCache

--- web/app_dev.php
+++ web/app_dev.php
@@ -20,7 +20,6 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 }
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
