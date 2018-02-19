a4eecfdd6b5da923dbbe8ce420d0a650e3b38a01
Jon Cram <jon@webignition.net>
2018-02-19 14:27:26 +0000

    Update app entry points

--- web/app.php
+++ web/app.php
@@ -1,23 +1,20 @@
 <?php
 
-use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 umask(0000);
 
+/**
+ * @var Composer\Autoload\ClassLoader
+ */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
-
-// Use APC for autoloading to improve performance
-// Change 'sf2' by the prefix you want in order to prevent key conflict with another application
-/*
-$loader = new ApcClassLoader('sf2', $loader);
-$loader->register(true);
-*/
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 $kernel = new AppCache($kernel);
+
+Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
