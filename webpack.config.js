484cc53472b7aae796686ea6c8a15ca87b304481
Jon Cram <webignition@gmail.com>
2018-04-19 17:20:04 +0100

    Move CSS to webpack encore: navbar styling (#243)

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ Encore
 
     // will create web/build/app.js and web/build/app.css
     .addEntry('app', './assets/js/app.js')
+    .addStyleEntry('single-page-dialog', './assets/css/single-page-dialog.scss')
 
     // allow sass/scss files to be processed
     .enableSassLoader()
