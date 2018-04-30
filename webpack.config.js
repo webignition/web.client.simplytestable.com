12f4adc23e6544b0469f145c1425d15b9955c2da
Jon Cram <webignition@gmail.com>
2018-04-30 12:36:12 +0100

    Move CSS to webpack encore: dashboard (#250)

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ Encore
     // will create web/build/app.js and web/build/app.css
     .addEntry('app', './assets/js/app.js')
     .addStyleEntry('single-page-dialog', './assets/css/single-page-dialog.scss')
+    .addStyleEntry('dashboard', './assets/css/dashboard.scss')
 
     // allow sass/scss files to be processed
     .enableSassLoader()
