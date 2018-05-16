71ef32dd784e87256a91a95ce728e5a40cd9e6a7
Jon Cram <jon@webignition.net>
2018-05-16 18:35:46 +0100

    Move CSS to webpack encore: single-page-dialog

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@ Encore
 
     // will create web/build/app.js and web/build/app.css
     .addEntry('app', './assets/js/app.js')
-    .addStyleEntry('single-page-dialog', './assets/css/single-page-dialog.scss')
     .addStyleEntry('dashboard', './assets/css/dashboard.scss')
 
     // allow sass/scss files to be processed
