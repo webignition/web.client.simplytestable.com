5be37a05f0f76c970fc811c75f7c245f6e00e126
Jon Cram <webignition@gmail.com>
2018-05-24 15:15:00 +0100

    Replace bootstrap affix usage with position: sticky (#289)

--- src/SimplyTestable/WebClientBundle/Resources/assets/bs3/js/user-account.js
+++ src/SimplyTestable/WebClientBundle/Resources/assets/bs3/js/user-account.js
@@ -32,4 +32,11 @@ $(document).ready(function() {
         }
     }
 
+    var stickyNav = document.querySelector('.js-sticky-nav');
+
+    if (document.querySelector('.csspositionsticky')) {
+        stickyNav.classList.remove('js-sticky-nav');
+    } else {
+        Stickyfill.addOne(stickyNav);
+    }
 });
