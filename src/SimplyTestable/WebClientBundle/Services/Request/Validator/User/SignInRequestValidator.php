cd6b9b4b4d4c6e47c1fd9756df8fe30c45027ae2
Jon Cram <jon@webignition.net>
2018-05-29 07:40:17 +0100

    Create UserAccountRequestValidator

--- src/SimplyTestable/WebClientBundle/Services/Request/Validator/User/SignInRequestValidator.php
+++ src/SimplyTestable/WebClientBundle/Services/Request/Validator/User/SignInRequestValidator.php
@@ -2,109 +2,34 @@
 
 namespace SimplyTestable\WebClientBundle\Services\Request\Validator\User;
 
-use Egulias\EmailValidator\EmailValidator;
+use SimplyTestable\WebClientBundle\Request\User\AbstractUserAccountRequest;
 use SimplyTestable\WebClientBundle\Request\User\SignInRequest;
 use SimplyTestable\WebClientBundle\Services\SystemUserService;
 use webignition\SimplyTestableUserModel\User;
 
-class SignInRequestValidator
+class SignInRequestValidator extends UserAccountRequestValidator
 {
-    const STATE_EMPTY = 'empty';
-    const STATE_INVALID = 'invalid';
     const STATE_PUBLIC_USER = 'public-user';
 
     /**
-     * @var EmailValidator
+     * @param AbstractUserAccountRequest $userAccountRequest
      */
-    private $emailValidator;
-
-    /**
-     * @var bool
-     */
-    private $isValid;
-
-    /**
-     * @var string
-     */
-    private $invalidFieldName;
-
-    /**
-     * @var string
-     */
-    private $invalidFieldState;
-
-    /**
-     * @param EmailValidator $emailValidator
-     */
-    public function __construct(EmailValidator $emailValidator)
+    public function validate(AbstractUserAccountRequest $userAccountRequest)
     {
-        $this->emailValidator = $emailValidator;
-    }
+        parent::validate($userAccountRequest);
 
-    /**
-     * @param SignInRequest $signInRequest
-     */
-    public function validate(SignInRequest $signInRequest)
-    {
-        $this->isValid = true;
-        $email = $signInRequest->getEmail();
-
-        if (empty($email)) {
-            $this->isValid = false;
-            $this->invalidFieldName = SignInRequest::PARAMETER_EMAIL;
-            $this->invalidFieldState = self::STATE_EMPTY;
-
+        if (!$this->getIsValid()) {
             return;
         }
 
-        if (!$this->emailValidator->isValid($email)) {
-            $this->isValid = false;
-            $this->invalidFieldName = SignInRequest::PARAMETER_EMAIL;
-            $this->invalidFieldState = self::STATE_INVALID;
-
-            return;
-        }
-
-        $password = $signInRequest->getPassword();
-        if (empty($password)) {
-            $this->isValid = false;
-            $this->invalidFieldName = SignInRequest::PARAMETER_PASSWORD;
-            $this->invalidFieldState = self::STATE_EMPTY;
-
-            return;
-        }
+        $email = $userAccountRequest->getEmail();
+        $password = $userAccountRequest->getPassword();
 
         $user = new User($email, $password);
         if (SystemUserService::isPublicUser($user)) {
-            $this->isValid = false;
-            $this->invalidFieldName = SignInRequest::PARAMETER_EMAIL;
-            $this->invalidFieldState = self::STATE_PUBLIC_USER;
+            $this->invalidate(SignInRequest::PARAMETER_EMAIL, self::STATE_PUBLIC_USER);
 
             return;
         }
-    }
-
-    /**
-     * @return bool
-     */
-    public function getIsValid()
-    {
-        return $this->isValid;
-    }
-
-    /**
-     * @return string
-     */
-    public function getInvalidFieldName()
-    {
-        return $this->invalidFieldName;
-    }
-
-    /**
-     * @return string
-     */
-    public function getInvalidFieldState()
-    {
-        return $this->invalidFieldState;
     }
 }
