d0d135855a69ba2f6b6b5db7064a3400f4b26393
Jon Cram <jon@webignition.net>
2018-05-30 08:20:19 +0100

    Update modal launching

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,10 +4,12 @@ require('classlist-polyfill');
 
 let formButtonSpinner = require('./form-button-spinner');
 let formFieldFocuser = require('./form-field-focuser');
+let modalControl = require('./modal-control');
 
 const onDomContentLoaded = function () {
     formButtonSpinner(document.querySelectorAll('.js-form-button-spinner'));
     formFieldFocuser(document.querySelectorAll('[data-focused]'));
+    modalControl(document.querySelectorAll('.modal-control'));
 };
 
 document.addEventListener('DOMContentLoaded', onDomContentLoaded);
