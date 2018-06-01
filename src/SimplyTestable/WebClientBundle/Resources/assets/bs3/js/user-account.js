0b24d986dd0b4e8a5063a7e88d6d5a5f98d630fa
Jon Cram <webignition@gmail.com>
2018-06-01 15:27:05 +0100

    Move JS to webpack encore: user-account.js (#302)

--- src/SimplyTestable/WebClientBundle/Resources/assets/bs3/js/user-account.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    var stickyNav = document.querySelector('.js-sticky-nav');
-
-    if (document.querySelector('.csspositionsticky')) {
-        stickyNav.classList.remove('js-sticky-nav');
-    } else {
-        Stickyfill.addOne(stickyNav);
-    }
-});
