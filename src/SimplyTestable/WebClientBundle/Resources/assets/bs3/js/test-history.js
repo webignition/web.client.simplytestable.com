3c63aeff1582f3738ee4fb393341fad26611e0c4
Jon Cram <webignition@gmail.com>
2018-06-15 18:55:11 +0100

    Move JS to webpack encore: test-history (#311)

--- src/SimplyTestable/WebClientBundle/Resources/assets/bs3/js/test-history.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    if ($('.requires-results').length > 0) {
-        $('.requires-results').each(function () {
-            $(this).stResultPreparer().init();
-        });
-    }
-});
