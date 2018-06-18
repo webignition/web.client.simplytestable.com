24858c017a7062feaddcd8e539a6dfde3dbed927
Jon Cram <jon@webignition.net>
2018-06-18 08:32:01 +0100

    Implement on test results page

--- assets/js/page/test-results.js
+++ assets/js/page/test-results.js
@@ -1,5 +1,19 @@
 let unavailableTaskTypeModalLauncher = require('../unavailable-task-type-modal-launcher');
+let HttpAuthenticationOptionsFactory = require('../services/http-authentication-options-factory');
 
-module.exports = function (document) {
-    unavailableTaskTypeModalLauncher(document.querySelectorAll('.task-type-option.not-available'));
-};
+class TestResults {
+    /**
+     * @param {Document} document
+     */
+    constructor (document) {
+        this.document = document;
+        this.httpAuthenticationOptions = HttpAuthenticationOptionsFactory.create(document.querySelector('.http-authentication-test-option'));
+    }
+
+    init () {
+        unavailableTaskTypeModalLauncher(this.document.querySelectorAll('.task-type-option.not-available'));
+        this.httpAuthenticationOptions.init();
+    };
+}
+
+module.exports = TestResults;
