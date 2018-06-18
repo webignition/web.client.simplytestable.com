6f85901051a8fdbfc36f4a4014732c10ca9dd96a
Jon Cram <jon@webignition.net>
2018-06-18 17:43:25 +0100

    Make test lock/unlock js-only

---
+++ assets/js/model/element/test-lock-unlock.js
@@ -0,0 +1,71 @@
+let HttpClient = require('../../services/http-client');
+
+class TestLockUnlock {
+    constructor (element) {
+        this.element = element;
+        this.state = element.getAttribute('data-state');
+        this.data = {
+            locked: JSON.parse(element.getAttribute('data-locked')),
+            unlocked: JSON.parse(element.getAttribute('data-unlocked'))
+        };
+        this.icon = element.querySelector('.fa');
+        this.action = element.querySelector('.action');
+        this.description = element.querySelector('.description');
+
+        this.iconClasses = [
+            this.data.locked.icon,
+            this.data.unlocked.icon
+        ];
+    }
+
+    init () {
+        this.element.classList.remove('invisible');
+        this._render();
+
+        this.element.addEventListener('click', this._clickEventListener.bind(this));
+        this.element.addEventListener(HttpClient.getRetrievedEventName(), () => {
+            this.element.classList.remove('de-emphasize');
+            this.icon.classList.remove('fa-spin', 'fa-spinner');
+            this._toggle();
+        });
+    };
+
+    _toggle () {
+        this.state = this.state === 'locked' ? 'unlocked' : 'locked';
+        this._render();
+    };
+
+    _render () {
+        this._removeStateIcons();
+
+        let stateData = this.data[this.state];
+        this.icon.classList.add(this._createIconClassName(stateData.icon));
+        this.action.innerText = stateData.action;
+        this.description.innerText = stateData.description;
+    };
+
+    _removeStateIcons () {
+        this.iconClasses.forEach((iconClass) => {
+            this.icon.classList.remove(this._createIconClassName(iconClass));
+        });
+    };
+
+    _clickEventListener () {
+        this._removeStateIcons();
+        this.element.classList.add('de-emphasize');
+        this.icon.classList.add('fa-spin', 'fa-spinner');
+
+        HttpClient.post(this.data[this.state].url, this.element, 'default');
+    };
+
+    /**
+     * @param {string} name
+     * @returns {string}
+     * @private
+     */
+    _createIconClassName (name) {
+        return 'fa-' + name;
+    }
+}
+
+module.exports = TestLockUnlock;
--- assets/js/page/test-results.js
+++ assets/js/page/test-results.js
@@ -1,6 +1,7 @@
 let unavailableTaskTypeModalLauncher = require('../unavailable-task-type-modal-launcher');
 let HttpAuthenticationOptionsFactory = require('../services/http-authentication-options-factory');
 let CookieOptionsFactory = require('../services/cookie-options-factory');
+let TestLockUnlock = require('../model/element/test-lock-unlock');
 
 class TestResults {
     /**
@@ -10,12 +11,14 @@ class TestResults {
         this.document = document;
         this.httpAuthenticationOptions = HttpAuthenticationOptionsFactory.create(document.querySelector('.http-authentication-test-option'));
         this.cookieOptions = CookieOptionsFactory.create(document.querySelector('.cookies-test-option'));
+        this.testLockUnlock = new TestLockUnlock(document.querySelector('.btn-lock-unlock'));
     }
 
     init () {
         unavailableTaskTypeModalLauncher(this.document.querySelectorAll('.task-type-option.not-available'));
         this.httpAuthenticationOptions.init();
         this.cookieOptions.init();
+        this.testLockUnlock.init();
     };
 }
 
