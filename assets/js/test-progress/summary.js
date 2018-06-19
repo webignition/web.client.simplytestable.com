f39ef276441463eb8dce1fd791e2490d838f9fad
Jon Cram <jon@webignition.net>
2018-06-19 12:51:03 +0100

    Retrieve test summary data

--- assets/js/test-progress/summary.js
+++ assets/js/test-progress/summary.js
@@ -1,21 +1,31 @@
 let FormButton = require('../model/element/form-button');
+let HttpClient = require('../services/http-client');
 
 class Summary {
     /**
      * @param {Element} element
      */
     constructor (element) {
+        this.element = element;
+        this.sourceUrl = element.getAttribute('data-source-url');
         this.cancelAction = new FormButton(element.querySelector('.cancel-action'));
         this.cancelCrawlAction = new FormButton(element.querySelector('.cancel-crawl-action'));
+        this.summaryData = null;
     }
 
     init () {
         this._addEventListeners();
+        this._update();
     };
 
     _addEventListeners () {
+        this.element.addEventListener(HttpClient.getRetrievedEventName(), this._httpRequestRetrievedEventListener.bind(this));
         this.cancelAction.element.addEventListener('click', this._cancelActionClickEventListener.bind(this));
         this.cancelCrawlAction.element.addEventListener('click', this._cancelCrawlActionClickEventListener.bind(this));
+    };
+
+    _httpRequestRetrievedEventListener (event) {
+        this.summaryData = event.detail.response;
     };
 
     _cancelActionClickEventListener () {
@@ -26,6 +36,15 @@ class Summary {
     _cancelCrawlActionClickEventListener () {
         this.cancelCrawlAction.markAsBusy();
         this.cancelCrawlAction.deEmphasize();
+    };
+
+    _update () {
+        let now = new Date();
+        let updateUrl = this.sourceUrl + '?timestamp=' + now.getTime();
+
+        console.log(updateUrl);
+
+        HttpClient.getJson(updateUrl, this.element, 'default');
     };
 }
 
