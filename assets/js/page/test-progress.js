406e3f42c90dd65da9e1f57569237403adde670f
Jon Cram <jon@webignition.net>
2018-06-19 19:08:15 +0100

    Move data retrieval to test progress main

--- assets/js/page/test-progress.js
+++ assets/js/page/test-progress.js
@@ -1,11 +1,13 @@
 let Summary = require('../test-progress/summary');
 let TestAlertContainer = require('../model/element/test-alert-container');
+let HttpClient = require('../services/http-client');
 
 class TestProgress {
     /**
      * @param {Document} document
      */
     constructor (document) {
+        this.document = document;
         this.summary = new Summary(document.querySelector('.js-summary'));
         this.alertContainer = new TestAlertContainer(document.querySelector('.alert-container'));
     }
@@ -16,6 +18,34 @@ class TestProgress {
         this.summary.element.addEventListener(Summary.getRenderAmmendmentEventName(), (event) => {
             this.alertContainer.renderUrlLimitNotification();
         });
+
+        this.document.body.addEventListener(HttpClient.getRetrievedEventName(), this._httpRequestRetrievedEventListener.bind(this));
+
+        this._refreshSummary();
+    };
+
+    _httpRequestRetrievedEventListener (event) {
+        if (event.detail.requestId === 'test-progress.summary.update') {
+            if (event.detail.request.responseURL.indexOf(window.location.toString()) !== 0) {
+                this.summary.progressBar.setCompletionPercent(100);
+                window.location.href = event.detail.request.responseURL;
+
+                return;
+            }
+
+            this.summary.render(event.detail.response);
+        }
+
+        window.setTimeout(() => {
+            this._refreshSummary();
+        }, 3000);
+    };
+
+    _refreshSummary () {
+        let summaryUrl = this.document.body.getAttribute('data-summary-url');
+        let now = new Date();
+
+        HttpClient.getJson(summaryUrl + '?timestamp=' + now.getTime(), this.document.body, 'test-progress.summary.update');
     };
 }
 
--- assets/js/test-progress/summary.js
+++ assets/js/test-progress/summary.js
@@ -1,7 +1,6 @@
 let FormButton = require('../model/element/form-button');
 let ProgressBar = require('../model/element/progress-bar');
 let TaskQueues = require('../model/element/task-queues');
-let HttpClient = require('../services/http-client');
 
 class Summary {
     /**
@@ -9,18 +8,15 @@ class Summary {
      */
     constructor (element) {
         this.element = element;
-        this.sourceUrl = element.getAttribute('data-source-url');
         this.cancelAction = new FormButton(element.querySelector('.cancel-action'));
         this.cancelCrawlAction = new FormButton(element.querySelector('.cancel-crawl-action'));
         this.progressBar = new ProgressBar(element.querySelector('.progress-bar'));
         this.stateLabel = element.querySelector('.js-state-label');
-        this.summaryData = null;
         this.taskQueues = new TaskQueues(element.querySelector('.task-queues'));
     }
 
     init () {
         this._addEventListeners();
-        this._update();
     };
 
     /**
@@ -31,27 +27,10 @@ class Summary {
     };
 
     _addEventListeners () {
-        this.element.addEventListener(HttpClient.getRetrievedEventName(), this._httpRequestRetrievedEventListener.bind(this));
         this.cancelAction.element.addEventListener('click', this._cancelActionClickEventListener.bind(this));
         this.cancelCrawlAction.element.addEventListener('click', this._cancelCrawlActionClickEventListener.bind(this));
     };
 
-    _httpRequestRetrievedEventListener (event) {
-        this.summaryData = event.detail.response;
-
-        if (event.detail.request.responseURL.indexOf(window.location.toString()) !== 0) {
-            this.progressBar.setCompletionPercent(100);
-            window.location.href = event.detail.request.responseURL;
-
-            return;
-        }
-
-        this._render();
-
-        window.setTimeout(() => {
-            this._update();
-        }, 3000);
-    };
     _cancelActionClickEventListener () {
         this.cancelAction.markAsBusy();
         this.cancelAction.deEmphasize();
@@ -62,19 +41,15 @@ class Summary {
         this.cancelCrawlAction.deEmphasize();
     };
 
-    _update () {
-        let now = new Date();
-        let updateUrl = this.sourceUrl + '?timestamp=' + now.getTime();
-
-        HttpClient.getJson(updateUrl, this.element, 'default');
-    };
-
-    _render () {
-        let remoteTest = this.summaryData.remote_test;
+    /**
+     * @param {object} summaryData
+     */
+    render (summaryData) {
+        let remoteTest = summaryData.remote_test;
 
         this.progressBar.setCompletionPercent(remoteTest.completion_percent);
-        this.progressBar.setStyle(this.summaryData.test.state === 'crawling' ? 'warning' : 'default');
-        this.stateLabel.innerText = this.summaryData.state_label;
+        this.progressBar.setStyle(summaryData.test.state === 'crawling' ? 'warning' : 'default');
+        this.stateLabel.innerText = summaryData.state_label;
         this.taskQueues.render(remoteTest.task_count, remoteTest.task_count_by_state);
 
         if (remoteTest.ammendments && remoteTest.ammendments.length > 0) {
