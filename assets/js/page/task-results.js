db6c44ceba8e322ca2409c5a7d3ced8d08fbe14f
Jon Cram <jon@webignition.net>
2018-06-21 19:28:41 +0100

    Move summary stat anchor click listener to function

--- assets/js/page/task-results.js
+++ assets/js/page/task-results.js
@@ -10,17 +10,27 @@ class TaskResults {
 
     init () {
         [].forEach.call(this.document.querySelectorAll('.summary-stats a'), (anchorElement) => {
-            anchorElement.addEventListener('click', (event) => {
-                event.preventDefault();
-
-                let targetId = anchorElement.getAttribute('href').replace('#', '');
-                let target = this.document.getElementById(targetId);
-
-                if (target) {
-                    ScrollTo.scrollTo(target, -50);
-                }
-            });
+            anchorElement.addEventListener('click', this._summaryStatsAnchorClickEventListener.bind(this));
         });
+    };
+
+    _summaryStatsAnchorClickEventListener (event) {
+        event.preventDefault();
+
+        let anchorElement = null;
+
+        event.path.forEach(function (pathElement) {
+            if (!anchorElement && pathElement.nodeName === 'A') {
+                anchorElement = pathElement;
+            }
+        });
+
+        let targetId = anchorElement.getAttribute('href').replace('#', '');
+        let target = this.document.getElementById(targetId);
+
+        if (target) {
+            ScrollTo.scrollTo(target, -50);
+        }
     };
 }
 
