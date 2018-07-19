1610eeaeefa41cc17c9e806589079024130bf710
Jon Cram <jon@webignition.net>
2018-07-19 12:11:22 +0100

    Update routing syntax for Stripe\EventController

---
+++ tests/WebClientBundle/Functional/Controller/AbstractControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\WebClientBundle\Functional\Controller;
+
+use Symfony\Component\Routing\RouterInterface;
+use Tests\WebClientBundle\Functional\AbstractBaseTestCase;
+
+class AbstractControllerTest extends AbstractBaseTestCase
+{
+    /**
+     * @var RouterInterface
+     */
+    protected $router;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->router = self::$container->get(RouterInterface::class);
+    }
+}
