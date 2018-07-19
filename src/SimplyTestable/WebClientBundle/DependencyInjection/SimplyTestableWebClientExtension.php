73aada175ff6fc65fc1830b22b96a39d06fb5245
Jon Cram <jon@webignition.net>
2018-07-19 08:41:19 +0100

    Move link-integrity-excluded-domains.yml

--- src/SimplyTestable/WebClientBundle/DependencyInjection/SimplyTestableWebClientExtension.php
+++ src/SimplyTestable/WebClientBundle/DependencyInjection/SimplyTestableWebClientExtension.php
@@ -22,7 +22,6 @@ class SimplyTestableWebClientExtension extends Extension
         'mail.yml',
         'plans.yml',
         'test_options.yml',
-        'link-integrity-excluded-domains.yml',
         'link_integrity_error_code_map.yml',
     ];
 
