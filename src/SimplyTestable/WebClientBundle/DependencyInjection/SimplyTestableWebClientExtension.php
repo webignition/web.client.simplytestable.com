c8a2a0f939d00b6ce21819335e3af2d4a4a5254a
Jon Cram <jon@webignition.net>
2018-07-19 08:44:42 +0100

    Move plans.yml

--- src/SimplyTestable/WebClientBundle/DependencyInjection/SimplyTestableWebClientExtension.php
+++ src/SimplyTestable/WebClientBundle/DependencyInjection/SimplyTestableWebClientExtension.php
@@ -19,7 +19,6 @@ class SimplyTestableWebClientExtension extends Extension
      */
     private $parameterFiles = [
         'task_types.yml',
-        'plans.yml',
         'test_options.yml',
     ];
 
