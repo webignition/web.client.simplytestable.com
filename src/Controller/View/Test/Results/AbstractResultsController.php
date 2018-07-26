76cc96439cea9bdc53871642fae05f010fecc271
Jon Cram <jon@webignition.net>
2018-07-26 10:43:31 +0100

    Remove RequiresCompletedTest interface method implementations

--- src/Controller/View/Test/Results/AbstractResultsController.php
+++ src/Controller/View/Test/Results/AbstractResultsController.php
@@ -5,55 +5,10 @@ namespace App\Controller\View\Test\Results;
 use App\Controller\AbstractBaseViewController;
 use App\Entity\Test\Test;
 use App\Model\RemoteTest\RemoteTest;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\RouterInterface;
 
 abstract class AbstractResultsController extends AbstractBaseViewController
 {
     const RESULTS_PREPARATION_THRESHOLD = 100;
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getFailedNoSitemapTestResponse(RouterInterface $router, Request $request)
-    {
-        return new RedirectResponse($router->generate(
-            'view_test_results_failed_no_urls_detected',
-            [
-                'website' => $request->attributes->get('website'),
-                'test_id' => $request->attributes->get('test_id')
-            ]
-        ));
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getRejectedTestResponse(RouterInterface $router, Request $request)
-    {
-        return new RedirectResponse($router->generate(
-            'view_test_results_rejected',
-            [
-                'website' => $request->attributes->get('website'),
-                'test_id' => $request->attributes->get('test_id')
-            ]
-        ));
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getNotFinishedTestResponse(RouterInterface $router, Request $request)
-    {
-        return new RedirectResponse($router->generate(
-            'view_test_progress',
-            [
-                'website' => $request->attributes->get('website'),
-                'test_id' => $request->attributes->get('test_id')
-            ]
-        ));
-    }
 
     /**
      * @param RemoteTest $remoteTest
