fd1cc8c869aa30e12526371973da4f8249a40fe9
Jon Cram <webignition@gmail.com>
2019-03-01 18:40:41 +0000

    Tidy up ::getFinishedCount() (#643)

--- src/Services/RemoteTestService.php
+++ src/Services/RemoteTestService.php
@@ -338,14 +338,9 @@ class RemoteTestService
         return $this->jsonResponseHandler->handle($response);
     }
 
-    /**
-     * @param string|null $filter
-     *
-     * @return int
-     */
-    public function getFinishedCount($filter = null)
+    public function getFinishedCount(?string $filter = null): int
     {
-        $finishedCount = null;
+        $finishedCount = 0;
 
         try {
             $response = $this->coreApplicationHttpClient->get(
