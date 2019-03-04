65e8ca03b126367ca5ab1d831a0e4ae6e3fd32db
Jon Cram <jon@webignition.net>
2019-03-04 14:52:55 +0000

    Update Controller/View/Test/Results/PreparingController

--- src/Controller/View/Test/Results/PreparingController.php
+++ src/Controller/View/Test/Results/PreparingController.php
@@ -72,8 +72,11 @@ class PreparingController extends AbstractBaseViewController
     public function indexAction(Request $request, $website, $test_id)
     {
         $test = $this->testService->get($website, $test_id);
-        $remoteTest = $this->remoteTestService->get();
+        if (empty($test)) {
+            return new RedirectResponse($this->generateUrl('view_dashboard'));
+        }
 
+        $remoteTest = $this->remoteTestService->get($test);
         if (empty($remoteTest)) {
             return new RedirectResponse($this->generateUrl('view_dashboard'));
         }
