31f3797eda7d905e830e76b30481eb71f41d4f66
Jon Cram <jon@webignition.net>
2019-03-04 17:56:30 +0000

    Remove RemoteTestService::getRecent(), ::getFinished()

--- src/Services/RemoteTestService.php
+++ src/Services/RemoteTestService.php
@@ -8,7 +8,6 @@ use App\Exception\CoreApplicationRequestException;
 use App\Exception\InvalidContentTypeException;
 use App\Exception\InvalidCredentialsException;
 use App\Model\RemoteTest\RemoteTest;
-use App\Model\TestList;
 use App\Model\TestOptions;
 use webignition\SimplyTestableUserInterface\UserInterface;
 
@@ -236,53 +235,6 @@ class RemoteTestService
     }
 
     /**
-     * @param int $limit
-     *
-     * @return TestList
-     *
-     * @throws CoreApplicationRequestException
-     * @throws InvalidContentTypeException
-     * @throws InvalidCredentialsException
-     */
-    public function getRecent(int $limit = 3): TestList
-    {
-        return $this->getList([
-            'limit' => $limit,
-            'offset' => 0,
-            'exclude-states' => [
-                'new',
-                'preparing',
-                'resolving',
-                'resolved',
-                'rejected',
-                'queued',
-            ],
-        ]);
-    }
-
-    /**
-     * @param int $limit
-     * @param int $offset
-     * @param string $filter
-     *
-     * @return TestList
-     *
-     * @throws CoreApplicationRequestException
-     * @throws InvalidContentTypeException
-     * @throws InvalidCredentialsException
-     */
-    public function getFinished(int $limit, int $offset, ?string $filter = null): TestList
-    {
-        return $this->getList([
-            'limit' => $limit,
-            'offset' => $offset,
-            'exclude-states' => ['rejected'],
-            'exclude-current' => 1,
-            'url-filter' => $filter,
-        ]);
-    }
-
-    /**
      * @return string[]
      *
      * @throws CoreApplicationRequestException
@@ -329,33 +281,6 @@ class RemoteTestService
         }
 
         return $finishedCount;
-    }
-
-    /**
-     * @param array $routeParameters
-     *
-     * @return TestList
-     *
-     * @throws CoreApplicationRequestException
-     * @throws InvalidContentTypeException
-     * @throws InvalidCredentialsException
-     */
-    private function getList(array $routeParameters): TestList
-    {
-        $list = new TestList();
-
-        $response = $this->coreApplicationHttpClient->get('tests_list', $routeParameters);
-        $responseData = $this->jsonResponseHandler->handle($response);
-
-        $list->setMaxResults($responseData['max_results']);
-        $list->setLimit($responseData['limit']);
-        $list->setOffset($responseData['offset']);
-
-        foreach ($responseData['jobs'] as $remoteTestData) {
-            $list->addRemoteTest(new RemoteTest($remoteTestData));
-        }
-
-        return $list;
     }
 
     public function retrieveLatest(string $canonicalUrl): ?RemoteTest
