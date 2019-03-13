52d960d492c8822927f6e47c1ad43810e67352d1
Jon Cram <jon@webignition.net>
2019-03-13 17:06:43 +0000

    Mock TestService, RemoteTestService in RedirectController tests

--- tests/Functional/Controller/RedirectControllerTest.php
+++ tests/Functional/Controller/RedirectControllerTest.php
@@ -3,7 +3,11 @@
 
 namespace App\Tests\Functional\Controller;
 
-use Doctrine\ORM\EntityManagerInterface;
+use App\Exception\CoreApplicationRequestException;
+use App\Model\RemoteTest\RemoteTest;
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\RequestInterface;
 use Psr\Log\LoggerInterface;
 use App\Controller\RedirectController;
 use App\Entity\Test;
@@ -17,6 +21,7 @@ use App\Tests\Services\HttpMockHandler;
 class RedirectControllerTest extends AbstractControllerTest
 {
     const USERNAME = 'user@example.com';
+    const WEBSITE = 'http://example.com/';
 
     /**
      * @var RedirectController
@@ -67,6 +72,7 @@ class RedirectControllerTest extends AbstractControllerTest
                 'user' => self::USERNAME,
                 'state' => Test::STATE_COMPLETED,
             ]),
+            HttpResponseFactory::createJsonResponse([]),
         ]);
 
         $this->client->request(
@@ -94,6 +100,7 @@ class RedirectControllerTest extends AbstractControllerTest
                 'user' => self::USERNAME,
                 'state' => Test::STATE_COMPLETED,
             ]),
+            HttpResponseFactory::createJsonResponse([]),
         ]);
 
         $this->client->request(
@@ -110,26 +117,19 @@ class RedirectControllerTest extends AbstractControllerTest
         $this->assertEquals('/http://example.com//1/', $response->getTargetUrl());
     }
 
+
     /**
-     * @dataProvider dataProviderForTestAction
+     * @dataProvider dataProviderForTestActionTaskResultsUrl
      */
-    public function testTestAction(
-        array $httpFixtures,
-        Request $request,
-        ?string $website,
-        ?int $testId,
+    public function testTestActionTaskResultsUrl(
+        string $website,
         string $expectedRedirectUrl
     ) {
-        $this->httpMockHandler->appendFixtures($httpFixtures);
-
         /* @var TestService $testService */
         $testService = self::$container->get(TestService::class);
 
         /* @var RemoteTestService $remoteTestService */
         $remoteTestService = self::$container->get(RemoteTestService::class);
-
-        /* @var EntityManagerInterface $entityManager */
-        $entityManager = self::$container->get(EntityManagerInterface::class);
 
         /* @var LoggerInterface $logger */
         $logger = self::$container->get(LoggerInterface::class);
@@ -138,10 +138,210 @@ class RedirectControllerTest extends AbstractControllerTest
         $response = $this->redirectController->testAction(
             $testService,
             $remoteTestService,
-            $entityManager,
+            $logger,
+            new Request(),
+            $website,
+            1
+        );
+
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+        $this->assertEquals($expectedRedirectUrl, $response->getTargetUrl());
+    }
+
+    public function dataProviderForTestActionTaskResultsUrl(): array
+    {
+        return [
+            'task results url without trailing slash' => [
+                'website' => 'http://example.com//1/2/results',
+                'expectedRedirectUrl' => '/website/http://example.com//test_id/1/task_id/2/results/',
+            ],
+            'task results url with trailing slash' => [
+                'website' => 'http://example.com//3/4/results/',
+                'expectedRedirectUrl' => '/website/http://example.com//test_id/3/task_id/4/results/',
+            ],
+        ];
+    }
+
+    public function testTestActionWebsiteOnlyUsesLatest()
+    {
+        $latestRemoteTest = new RemoteTest([
+            'id' => 99,
+            'website' => self::WEBSITE,
+        ]);
+
+        /* @var TestService $testService */
+        $testService = self::$container->get(TestService::class);
+
+        $remoteTestService = \Mockery::mock(RemoteTestService::class);
+        $remoteTestService
+            ->shouldReceive('retrieveLatest')
+            ->with(self::WEBSITE)
+            ->andReturn($latestRemoteTest);
+
+        /* @var LoggerInterface $logger */
+        $logger = self::$container->get(LoggerInterface::class);
+
+        /* @var RedirectResponse $response */
+        $response = $this->redirectController->testAction(
+            $testService,
+            $remoteTestService,
+            $logger,
+            new Request([], [
+                'website' => self::WEBSITE,
+            ]),
+            self::WEBSITE,
+            null
+        );
+
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+        $this->assertEquals('/http://example.com//99/', $response->getTargetUrl());
+    }
+
+    /**
+     * @dataProvider dataProviderForTestActionWebsiteOnlyNoMatchingTest
+     */
+    public function testTestActionWebsiteOnlyNoMatchingTest(Request $request)
+    {
+        /* @var TestService $testService */
+        $testService = self::$container->get(TestService::class);
+
+        $remoteTestService = \Mockery::mock(RemoteTestService::class);
+        $remoteTestService
+            ->shouldReceive('retrieveLatest')
+            ->with(self::WEBSITE)
+            ->andReturnNull();
+
+        /* @var LoggerInterface $logger */
+        $logger = self::$container->get(LoggerInterface::class);
+
+        /* @var RedirectResponse $response */
+        $response = $this->redirectController->testAction(
+            $testService,
+            $remoteTestService,
             $logger,
             $request,
-            $website,
+            self::WEBSITE,
+            null
+        );
+
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+        $this->assertEquals('/', $response->getTargetUrl());
+    }
+
+    public function dataProviderForTestActionWebsiteOnlyNoMatchingTest(): array
+    {
+        return [
+            'get website, no latest test, does not have test in repository' => [
+                'request' => new Request([
+                    'website' => self::WEBSITE,
+                ]),
+            ],
+            'no website, no latest test, does not have test in repository' => [
+                'request' => new Request(),
+            ],
+            'posted website, no latest test, does not have test in repository' => [
+                'request' => new Request([], [
+                    'website' => self::WEBSITE,
+                ]),
+            ],
+            'posted website, no scheme, no latest test, does not have test in repository' => [
+                'request' => new Request([], [
+                    'website' => 'example.com/',
+                ]),
+            ],
+            'posted website, no scheme, no host, no latest test, does not have test in repository' => [
+                'request' => new Request([], [
+                    'website' => '/',
+                ]),
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForTestActionWebsiteAndTestIdUnableToRetrieveLatest
+     */
+    public function testTestActionWebsiteAndTestIdUnableToRetrieveLatestTest(Request $request, ?int $testId)
+    {
+        $coreApplicationRequestException = new CoreApplicationRequestException(
+            new BadResponseException(
+                '',
+                \Mockery::mock(RequestInterface::class),
+                new Response(404)
+            )
+        );
+
+        $testService = \Mockery::mock(TestService::class);
+        $testService
+            ->shouldReceive('get')
+            ->andThrow($coreApplicationRequestException);
+
+        /* @var RemoteTestService $remoteTestService */
+        $remoteTestService = \Mockery::mock(RemoteTestService::class);
+
+        /* @var LoggerInterface $logger */
+        $logger = self::$container->get(LoggerInterface::class);
+
+        /* @var RedirectResponse $response */
+        $response = $this->redirectController->testAction(
+            $testService,
+            $remoteTestService,
+            $logger,
+            $request,
+            self::WEBSITE,
+            $testId
+        );
+
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+        $this->assertEquals('/http://example.com//', $response->getTargetUrl());
+    }
+
+    public function dataProviderForTestActionWebsiteAndTestIdUnableToRetrieveLatest(): array
+    {
+        return [
+            'posted website, no id, no latest test, does not have test in repository' => [
+                'request' => new Request([], [
+                    'website' => 'http://example.com/1/',
+                ]),
+                'testId' => null,
+            ],
+            'website and test_id, has latest test, http error retrieving remote test' => [
+                'request' => new Request(),
+                'testId' => 1,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForTestActionWebsiteAndTestHasTest
+     */
+    public function testTestActionWebsiteAndTestHasTest(
+        string $testState,
+        string $expectedRedirectUrl
+    ) {
+        $testId = 1;
+
+        $test = Test::create($testId, self::WEBSITE);
+        $test->setState($testState);
+
+        $testService = \Mockery::mock(TestService::class);
+        $testService
+            ->shouldReceive('get')
+            ->with(self::WEBSITE, $testId)
+            ->andReturn($test);
+
+        /* @var RemoteTestService $remoteTestService */
+        $remoteTestService = self::$container->get(RemoteTestService::class);
+
+        /* @var LoggerInterface $logger */
+        $logger = self::$container->get(LoggerInterface::class);
+
+        /* @var RedirectResponse $response */
+        $response = $this->redirectController->testAction(
+            $testService,
+            $remoteTestService,
+            $logger,
+            new Request(),
+            self::WEBSITE,
             $testId
         );
 
@@ -149,146 +349,16 @@ class RedirectControllerTest extends AbstractControllerTest
         $this->assertEquals($expectedRedirectUrl, $response->getTargetUrl());
     }
 
-    public function dataProviderForTestAction(): array
+    public function dataProviderForTestActionWebsiteAndTestHasTest(): array
     {
         return [
-            'task results url without trailing slash' => [
-                'httpFixtures' => [],
-                'request' => new Request(),
-                'website' => 'http://example.com//1/2/results',
-                'testId' => null,
-                'expectedRedirectUrl' => '/website/http://example.com//test_id/1/task_id/2/results/',
-            ],
-            'task results url with trailing slash' => [
-                'httpFixtures' => [],
-                'request' => new Request(),
-                'website' => 'http://example.com//3/4/results/',
-                'testId' => null,
-                'expectedRedirectUrl' => '/website/http://example.com//test_id/3/task_id/4/results/',
-            ],
-            'posted website, has latest test' => [
-                'httpFixtures' => [
-                    HttpResponseFactory::createJsonResponse([
-                        'id' => 99,
-                        'website' => 'http://example.com/'
-                    ]),
-                ],
-                'request' => new Request([], [
-                    'website' => 'http://example.com/',
-                ]),
-                'website' => 'http://example.com/',
-                'testId' => null,
-                'expectedRedirectUrl' => '/http://example.com//99/',
-            ],
-            'get website, no latest test, does not have test in repository' => [
-                'httpFixtures' => [
-                    HttpResponseFactory::createNotFoundResponse(),
-                ],
-                'request' => new Request([
-                    'website' => 'http://example.com/',
-                ]),
-                'website' => 'http://example.com/',
-                'testId' => null,
-                'expectedRedirectUrl' => '/',
-            ],
-            'no website, no latest test, does not have test in repository' => [
-                'httpFixtures' => [
-                    HttpResponseFactory::createNotFoundResponse(),
-                ],
-                'request' => new Request(),
-                'website' => 'http://example.com/',
-                'testId' => null,
-                'expectedRedirectUrl' => '/',
-            ],
-            'posted website, no latest test, does not have test in repository' => [
-                'httpFixtures' => [
-                    HttpResponseFactory::createNotFoundResponse(),
-                ],
-                'request' => new Request([], [
-                    'website' => 'http://example.com/',
-                ]),
-                'website' => 'http://example.com/',
-                'testId' => null,
-                'expectedRedirectUrl' => '/',
-            ],
-            'posted website, no scheme, no latest test, does not have test in repository' => [
-                'httpFixtures' => [
-                    HttpResponseFactory::createNotFoundResponse(),
-                ],
-                'request' => new Request([], [
-                    'website' => 'example.com/',
-                ]),
-                'website' => 'http://example.com/',
-                'testId' => null,
-                'expectedRedirectUrl' => '/',
-            ],
-            'posted website, no scheme, no host, no latest test, does not have test in repository' => [
-                'httpFixtures' => [
-                    HttpResponseFactory::createNotFoundResponse(),
-                ],
-                'request' => new Request([], [
-                    'website' => '/',
-                ]),
-                'website' => 'http://example.com/',
-                'testId' => null,
-                'expectedRedirectUrl' => '/',
-            ],
-            'posted website, no id, no latest test, does not have test in repository' => [
-                'httpFixtures' => [
-                    HttpResponseFactory::createNotFoundResponse(),
-                ],
-                'request' => new Request([], [
-                    'website' => 'http://example.com/1/',
-                ]),
-                'website' => 'http://example.com/',
-                'testId' => null,
-                'expectedRedirectUrl' => '/http://example.com//',
-            ],
-            'website and test_id, has latest test, http error retrieving remote test' => [
-                'httpFixtures' => [
-                    HttpResponseFactory::createNotFoundResponse(),
-                ],
-                'request' => new Request(),
-                'website' => 'http://example.com/',
-                'testId' => 1,
-                'expectedRedirectUrl' => '/http://example.com//',
-            ],
             'website and test_id, has latest test, success retrieving remote test, test finished' => [
-                'httpFixtures' => [
-                    HttpResponseFactory::createJsonResponse([
-                        'id' => 1,
-                        'website' => 'http://example.com/',
-                        'task_types' => [],
-                        'user' => self::USERNAME,
-                        'state' => Test::STATE_COMPLETED,
-                    ]),
-                ],
-                'request' => new Request(),
-                'website' => 'http://example.com/',
-                'testId' => 1,
+                'testState' => Test::STATE_COMPLETED,
                 'expectedRedirectUrl' => '/http://example.com//1/results/',
             ],
             'website and test_id, has latest test, success retrieving remote test, test in progress' => [
-                'httpFixtures' => [
-                    HttpResponseFactory::createJsonResponse([
-                        'id' => 1,
-                        'website' => 'http://example.com/',
-                        'task_types' => [],
-                        'user' => self::USERNAME,
-                        'state' => Test::STATE_IN_PROGRESS,
-                    ]),
-                ],
-                'request' => new Request(),
-                'website' => 'http://example.com/',
-                'testId' => 1,
+                'testState' => Test::STATE_IN_PROGRESS,
                 'expectedRedirectUrl' => '/http://example.com//1/progress/',
-            ],
-            'no website, no test id' => [
-                'httpFixtures' => [],
-                'request' => new Request(),
-                'website' => null,
-                'testId' => null,
-                'expectedRedirectUrl' => '/',
             ],
         ];
     }
