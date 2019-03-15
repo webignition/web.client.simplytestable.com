f4c4b88eff053f4ae64d817bd2ff78813d81322f
Jon Cram <jon@webignition.net>
2019-03-15 16:10:36 +0000

    Create TestFactory

---
+++ src/Services/TestFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\Test as TestEntity;
+use App\Model\RemoteTest\RemoteTest;
+use App\Model\Test as TestModel;
+
+class TestFactory
+{
+    public function create(TestEntity $entity, RemoteTest $remoteTest): TestModel
+    {
+        return new TestModel(
+            $entity,
+            $remoteTest->getWebsite(),
+            $remoteTest->getUser(),
+            $remoteTest->getState(),
+            $remoteTest->getType(),
+            $remoteTest->getTaskTypes(),
+            $remoteTest->getUrlCount()
+        );
+    }
+}
