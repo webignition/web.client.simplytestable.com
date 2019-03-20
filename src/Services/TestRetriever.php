36ae96bb0e4fe9e09080eee96a9e61108537f81b
Jon Cram <jon@webignition.net>
2019-03-20 12:28:35 +0000

    Create TestRetriever

---
+++ src/Services/TestRetriever.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\Test as TestEntity;
+use App\Model\Test as TestModel;
+use App\Exception\InvalidCredentialsException;
+use Doctrine\ORM\EntityManagerInterface;
+
+class TestRetriever
+{
+    const STATE_STARTING = 'new';
+    const STATE_CANCELLED = 'cancelled';
+    const STATE_COMPLETED = 'completed';
+    const STATE_IN_PROGRESS = 'in-progress';
+    const STATE_PREPARING = 'preparing';
+    const STATE_QUEUED = 'queued';
+    const STATE_FAILED_NO_SITEMAP = 'failed-no-sitemap';
+    const STATE_REJECTED = 'rejected';
+    const STATE_RESOLVING = 'resolving';
+    const STATE_RESOLVED = 'resolved';
+
+    private $entityManager;
+    private $taskService;
+    private $remoteTestService;
+    private $testFactory;
+
+    /**
+     * @var string[]
+     */
+    private $preparedStates = [
+        self::STATE_QUEUED,
+        self::STATE_IN_PROGRESS,
+        self::STATE_COMPLETED,
+        self::STATE_CANCELLED,
+    ];
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        TaskService $taskService,
+        RemoteTestService $remoteTestService,
+        TestFactory $testFactory
+    ) {
+        $this->entityManager = $entityManager;
+        $this->taskService = $taskService;
+        $this->remoteTestService = $remoteTestService;
+        $this->testFactory = $testFactory;
+    }
+
+    public function retrieve(int $testId): ?TestModel
+    {
+        $testRepository = $this->entityManager->getRepository(TestEntity::class);
+        $entity = $testRepository->findOneBy([
+            'testId' => $testId
+        ]);
+
+        if (empty($entity)) {
+            $entity = TestEntity::create((int) $testId);
+        }
+
+        $remoteTestData = null;
+
+        try {
+            $remoteTestData = $this->remoteTestService->getSummaryData($entity->getTestId());
+        } catch (InvalidCredentialsException $invalidCredentialsException) {
+            return null;
+        }
+
+        if (empty($remoteTestData)) {
+            return null;
+        }
+
+        $testModel = $this->testFactory->create($entity, $remoteTestData);
+
+        if (in_array($testModel->getState(), $this->preparedStates) && empty($testModel->getTaskIds())) {
+            $remoteTaskIds = $this->taskService->retrieveRemoteTaskIds($entity->getTestId());
+            $entity->setTaskIdCollection(implode(',', $remoteTaskIds));
+        }
+
+        $this->entityManager->persist($entity);
+        $this->entityManager->flush();
+
+        return $testModel;
+    }
+}
