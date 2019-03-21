1d6737d3269456abd4d7376258e8ec718fd4a78a
Jon Cram <webignition@gmail.com>
2019-03-21 16:36:07 +0000

    Remove TestService::get() (#929)

--- src/Services/TestService.php
+++ src/Services/TestService.php
@@ -2,15 +2,8 @@
 
 namespace App\Services;
 
-use App\Exception\InvalidContentTypeException;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\EntityRepository;
-use Psr\Log\LoggerInterface;
 use App\Entity\Test;
-use App\Exception\CoreApplicationRequestException;
-use App\Exception\InvalidCredentialsException;
-use App\Model\RemoteTest\RemoteTest;
-use webignition\NormalisedUrl\NormalisedUrl;
 
 class TestService
 {
@@ -25,58 +18,12 @@ class TestService
     const STATE_RESOLVING = 'resolving';
     const STATE_RESOLVED = 'resolved';
 
-    /**
-     * @var LoggerInterface
-     */
-    private $logger;
-
-    /**
-     * @var EntityManagerInterface
-     */
     private $entityManager;
-
-    /**
-     * @var EntityRepository
-     */
     private $testRepository;
 
-    /**
-     * @var TaskService
-     */
-    private $taskService;
-
-    /**
-     * @var RemoteTestService
-     */
-    private $remoteTestService;
-
-    /**
-     * @var string[]
-     */
-    private $preparedStates = [
-        self::STATE_QUEUED,
-        self::STATE_IN_PROGRESS,
-        self::STATE_COMPLETED,
-        self::STATE_CANCELLED,
-    ];
-
-    /**
-     * @param EntityManagerInterface $entityManager
-     * @param LoggerInterface $logger
-     * @param TaskService $taskService
-     * @param RemoteTestService $remoteTestService
-     */
-    public function __construct(
-        EntityManagerInterface $entityManager,
-        LoggerInterface $logger,
-        TaskService $taskService,
-        RemoteTestService $remoteTestService
-    ) {
+    public function __construct(EntityManagerInterface $entityManager)
+    {
         $this->entityManager = $entityManager;
-        $this->logger = $logger;
-        $this->taskService = $taskService;
-        $this->remoteTestService = $remoteTestService;
-
         $this->testRepository = $this->entityManager->getRepository(Test::class);
     }
 
@@ -94,58 +41,5 @@ class TestService
         }
 
         return $test;
-    }
-
-    /**
-     * @param int $testId
-     *
-     * @return Test|null
-     *
-     * @throws CoreApplicationRequestException
-     * @throws InvalidCredentialsException
-     * @throws InvalidContentTypeException
-     */
-    public function get(int $testId): ?Test
-    {
-        $test = $this->testRepository->findOneBy([
-            'testId' => $testId
-        ]);
-
-        if (empty($test)) {
-            $test = Test::create((int) $testId);
-        }
-
-        try {
-            $remoteTest = $this->remoteTestService->get($test->getTestId());
-        } catch (InvalidCredentialsException $invalidCredentialsException) {
-            return null;
-        }
-
-        if (!$remoteTest instanceof RemoteTest) {
-            return null;
-        }
-
-        $this->hydrateFromRemoteTest($test, $remoteTest);
-
-        if (in_array($test->getState(), $this->preparedStates) && !$test->hasTaskIds()) {
-            $remoteTaskIds = $this->taskService->retrieveRemoteTaskIds($test->getTestId());
-            $test->setTaskIdCollection(implode(',', $remoteTaskIds));
-        }
-
-        $this->entityManager->persist($test);
-        $this->entityManager->flush();
-
-        return $test;
-    }
-
-    private function hydrateFromRemoteTest(Test $test, RemoteTest $remoteTest)
-    {
-        $test->setUser($remoteTest->getUser());
-        $test->setWebsite(new NormalisedUrl($remoteTest->getWebsite()));
-        $test->setState($remoteTest->getState());
-        $test->setUrlCount($remoteTest->getUrlCount());
-        $test->setTestId($remoteTest->getId());
-        $test->setType((string) $remoteTest->getType());
-        $test->setTaskTypes($remoteTest->getTaskTypes());
     }
 }
