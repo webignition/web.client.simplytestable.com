c920a844e85d140063d3e3596b876e7352790396
Jon Cram <webignition@gmail.com>
2019-03-22 18:57:30 +0000

    History page filter auto-suggest is not applied when selected (#963)

--- assets/js/page/test-history.js
+++ assets/js/page/test-history.js
@@ -1,21 +1,24 @@
-let Modal = require('../test-history/modal');
+let UrlFilterForm = require('../test-history/url-filter-form');
 let Suggestions = require('../test-history/suggestions');
 let ListedTestCollection = require('../model/listed-test-collection');
+let Modal = require('../test-history/modal');
 
 module.exports = function (document) {
     const modalId = 'filter-options-modal';
     const filterOptionsSelector = '.action-badge-filter-options';
-    const modalElement = document.getElementById(modalId);
+    const urlFilterFormElement = document.getElementById(modalId);
     let filterOptionsElement = document.querySelector(filterOptionsSelector);
 
-    let modal = new Modal(modalElement);
-    let suggestions = new Suggestions(document, modalElement.getAttribute('data-source-url'));
+    let urlFilterForm = new UrlFilterForm(urlFilterFormElement);
+    let suggestions = new Suggestions(document, urlFilterFormElement.getAttribute('data-source-url'));
+    let modalControl = document.querySelector('.filter-modal-control');
+    let modal = new Modal(urlFilterFormElement);
 
     /**
      * @param {CustomEvent} event
      */
     let suggestionsLoadedEventListener = function (event) {
-        modal.setSuggestions(event.detail);
+        urlFilterForm.setSuggestions(event.detail);
         filterOptionsElement.classList.add('visible');
     };
 
@@ -37,12 +40,28 @@ module.exports = function (document) {
     };
 
     document.addEventListener(suggestions.loadedEventName, suggestionsLoadedEventListener);
-    modalElement.addEventListener(modal.filterChangedEventName, filterChangedEventListener);
+    urlFilterFormElement.addEventListener(urlFilterForm.filterChangedEventName, filterChangedEventListener);
 
     suggestions.retrieve();
 
     let listedTestCollection = ListedTestCollection.createFromNodeList(document.querySelectorAll('.listed-test'));
     listedTestCollection.forEach((listedTest) => {
         listedTest.enable();
+    });
+
+    modalControl.addEventListener('click', () => {
+        modal.show();
+    });
+
+    urlFilterFormElement.addEventListener(urlFilterForm.suggestionSelectedEventName, () => {
+        modal.allowClose = false;
+
+        window.setTimeout(() => {
+            modal.allowClose = true;
+        }, 100);
+    });
+
+    urlFilterFormElement.addEventListener(urlFilterForm.finishedEventName, () => {
+        modal.close();
     });
 };
