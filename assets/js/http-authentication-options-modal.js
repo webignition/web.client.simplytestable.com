a47744d812d5da93e78331be04dd6457d3572913
Jon Cram <webignition@gmail.com>
2019-04-04 10:59:57 +0100

    Don't try to read from http-auth form if not present (#986)

--- assets/js/http-authentication-options-modal.js
+++ assets/js/http-authentication-options-modal.js
@@ -1,15 +1,21 @@
 class HttpAuthenticationOptionsModal {
     constructor (element) {
-        this.document = element.ownerDocument;
-        this.element = element;
+        this.enabled = element !== null;
+
+        if (this.enabled) {
+            this.document = element.ownerDocument;
+            this.element = element;
+        }
     };
 
     init () {
-        this.element.addEventListener('show.bs.modal', () => {
-            [].forEach.call(this.element.querySelectorAll('.js-disable-readonly'), function (inputElement) {
-                inputElement.removeAttribute('readonly');
+        if (this.enabled) {
+            this.element.addEventListener('show.bs.modal', () => {
+                [].forEach.call(this.element.querySelectorAll('.js-disable-readonly'), function (inputElement) {
+                    inputElement.removeAttribute('readonly');
+                });
             });
-        });
+        }
     };
 }
 
